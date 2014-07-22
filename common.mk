4b57fbd89af91160540e24dcd888b009955ffeb8
TrustworthySystems <gatekeeper@sel4.systems>
2014-07-22 14:37:37 +1000

    Release snapshot

---
+++ common.mk
@@ -0,0 +1,271 @@
+#
+# Copyright 2014, NICTA
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(NICTA_BSD)
+#
+
+### CCACHE
+########################################
+# if ccache is in our path, use it!
+ifeq ($(CONFIG_BUILDSYS_USE_CCACHE),y)
+CCACHE=$(shell which ccache)
+else
+CCACHE=
+endif
+
+### Goanna
+ifeq (${CONFIG_BUILDSYS_USE_GOANNA},y)
+GOANNA=$(shell which goannacc)
+else
+GONNA=
+endif
+ifneq (${GOANNA},)
+GOANNA += --license-server=goanna.ken.nicta.com.au --silent-profile \
+    --profile=${CONFIG_BUILDSYS_GOANNA_PROFILE}
+endif
+
+### Verbose building
+########################################
+
+# Set V=1 for verbose building, this can be passed in on the command line
+# Set V=2 to have make echo out commands before executing them
+
+ifeq ($V, 1)
+	Q =
+else
+ifeq ($V, 2)
+	Q =
+else
+ifeq ($V, 3)
+	Q =
+else
+	Q = @
+endif
+endif
+endif
+
+CC  := $(CCACHE) $(if ${GOANNA},${GOANNA},$(TOOLPREFIX)gcc$(TOOLSUFFIX))
+ASM := $(CCACHE) $(TOOLPREFIX)gcc$(TOOLSUFFIX)
+LD  := $(CCACHE) $(TOOLPREFIX)ld$(TOOLSUFFIX)
+AR  := $(CCACHE) $(TOOLPREFIX)ar$(TOOLSUFFIX)
+CPP := $(CCACHE) $(TOOLPREFIX)cpp$(TOOLSUFFIX)
+OBJCOPY := $(CCACHE) $(TOOLPREFIX)objcopy$(TOOLSUFFIX)
+
+# Default path configuration (useful for local development)
+SEL4_LIBDIR     ?= $(STAGE_DIR)/lib
+SEL4_INCLUDEDIR ?= $(STAGE_DIR)/include
+SEL4_BINDIR     ?= $(STAGE_DIR)/bin
+SEL4_KERNEL     ?= $(STAGE_DIR)/kernel/kernel.elf
+
+# Compile configuration
+INCLUDE_PATH := $(SEL4_INCLUDEDIR) $(INCLUDE_DIRS)
+WARNINGS     := all
+
+# the NK_BUILD is used in the Makefile.flags file to distinguish
+# kernel build vs non-kernel build compiler options
+export NK_BUILD=y
+
+CFLAGS += $(INCLUDE_PATH:%=-I%)
+
+# Strip enclosing quotes.
+CONFIG_USER_CFLAGS:=$(patsubst %",%,$(patsubst "%,%,${CONFIG_USER_CFLAGS}))
+#")") Help syntax-highlighting editors.
+
+# We use start and end groups to recursively find symbols when linking.
+# However we cannot use these when performing link time optimisations.
+# We define these as variables here such that they can be overridden
+# if LTO is enabled
+STARTGROUP := -Wl,--start-group
+ENDGROUP := -Wl,--end-group
+
+ifeq (${CONFIG_USER_CFLAGS},)
+    CFLAGS += $(WARNINGS:%=-W%) -nostdinc -std=gnu99
+
+    ifeq (${CONFIG_USER_OPTIMISATION_Os},y)
+        CFLAGS += -Os
+    endif
+    ifeq (${CONFIG_USER_OPTIMISATION_O0},y)
+        CFLAGS += -O0
+    endif
+    ifeq (${CONFIG_USER_OPTIMISATION_O1},y)
+        CFLAGS += -O1
+    endif
+    ifeq (${CONFIG_USER_OPTIMISATION_O3},y)
+        CFLAGS += -O3
+    endif
+    ifeq (${CONFIG_USER_OPTIMISATION_O2},y)
+        CFLAGS += -O2
+    endif
+
+    ifeq (${CONFIG_LINK_TIME_OPTIMISATIONS},y)
+        CFLAGS += -flto
+        STARTGROUP :=
+        ENDGROUP :=
+    endif
+
+    CFLAGS += $(NK_CFLAGS)
+    CFLAGS += -fno-stack-protector
+else
+    # Override the cflags with Kconfig-specified flags
+	CFLAGS += ${CONFIG_USER_CFLAGS}
+endif
+
+ifeq (${CONFIG_WHOLE_PROGRAM_OPTIMISATIONS_USER},y)
+    LDFLAGS += -fwhole-program
+endif
+
+LIBGCC := -lgcc
+EH := $(shell $(CC) --print-file-name libgcc_eh.a)
+ifneq "${EH}" "libgcc_eh.a"
+  LIBGCC += -lgcc_eh
+endif
+
+
+LDFLAGS += $(NK_LDFLAGS) \
+		   $(SEL4_LIBDIR:%=-L%) \
+		   $(STARTGROUP) \
+		   $(LIBS:%=-l%) \
+		   $(LIBGCC) \
+		   $(ENDGROUP) \
+		   $(CFLAGS) \
+		   -static -nostdlib
+
+# Force _start to be linked in if need be - the user may already have it in a
+# .o file, otherwise this will pull it from a library such as
+# libsel4platsupport.
+LDFLAGS += -u _start
+
+# Object files
+OBJFILES = $(ASMFILES:%.S=%.o) $(CFILES:%.c=%.o) $(OFILES)
+
+# Copy a file.
+cp_file = \
+	@echo " [STAGE] $(notdir $2)"; cp -f $(1) $(2)
+
+# Where to look for header dependencies
+vpath %.h $(INCLUDE_PATH)
+
+# Where to look for library dependencies
+vpath %.a $(SEL4_LIBDIR)
+
+# Where to find the sources
+vpath %.c $(SOURCE_DIR)
+vpath %.S $(SOURCE_DIR)
+
+# Default is to build/install all targets
+default: $(PRIORITY_TARGETS) install-headers $(TARGETS)
+
+#
+# For each ".bin" or ".a" target, we also setup a rule to copy it into a final
+# binaries output directory.
+#
+default: $(patsubst %.bin,$(SEL4_BINDIR)/%,$(filter %.bin,$(TARGETS)))
+default: $(patsubst %.a,$(SEL4_LIBDIR)/%.a,$(filter %.a,$(TARGETS)))
+
+$(SEL4_BINDIR)/%: %.bin
+	$(Q)mkdir -p $(dir $@)
+	$(call cp_file,$<,$@)
+
+$(SEL4_LIBDIR)/%.a: %.a
+	$(Q)mkdir -p $(dir $@)
+	$(call cp_file,$<,$@)
+
+.PHONY: install-headers
+
+HDRFILES += $(srctree)/include/generated/autoconf.h
+
+install-headers:
+	@if [ -n "$(HDRFILES)" ] ; then \
+		mkdir -p $(SEL4_INCLUDEDIR) ; \
+		echo " [HEADERS]"; \
+		for file in $(HDRFILES); do \
+			echo -n " [STAGE] "; echo -n `basename $$file`; \
+			if [ -d $$file ]; then echo "/*"; else echo; fi; \
+			cp -aL $$file $(SEL4_INCLUDEDIR) ; \
+		done; \
+	fi
+	@if [ -n "$(RHDRFILES)" ] ; then \
+		mkdir -p $(SEL4_INCLUDEDIR) ; \
+		echo " [HEADERS] "; \
+		for hdrfile in $(RHDRFILES) ; do \
+			source=`echo "$$hdrfile" | sed 's/^\(.*\)[ \t][^ \t]*$$/\1/'` ; \
+			dest=$(SEL4_INCLUDEDIR)/`echo "$$hdrfile" | sed 's/^.*[ \t]\([^ \t]*\)$$/\1/'` ; \
+			mkdir -p $$dest; \
+			cp -a $$source $$dest ; \
+			echo -n " [STAGE]"; basename $$dest; \
+		done ; \
+	fi
+
+ifeq (${CONFIG_BUILDSYS_CPP_SEPARATE},y)
+%.o: %.c_pp
+	@echo " [CC] $@"
+	$(Q)mkdir -p $(dir $@)
+	$(Q)$(CC) -x c $(CFLAGS) -c $< -o $@
+else
+%.o: %.c $(HFILES) | install-headers
+	@echo " [CC] $@"
+	$(Q)mkdir -p $(dir $@)
+	$(Q)$(call make-depend,$<,$@,$(patsubst %.o,%.d,$@))
+	$(Q)$(CC) -x c $(CFLAGS) -c $< -o $@
+endif
+
+.PRECIOUS: %.c_pp
+%.c_pp: %.c $(HFILES) | install-headers
+	@echo " [CPP] $@"
+	$(Q)mkdir -p $(dir $@)
+	$(Q)$(call make-depend,$<,$@,$(patsubst %.c_pp,%.d,$@))
+	$(Q)$(CC) -E \
+        $(if ${CONFIG_BUILDSYS_CPP_RETAIN_COMMENTS},-C -CC,) \
+        $(if ${CONFIG_BUILDSYS_CPP_LINE_DIRECTIVES},,-P) \
+        $(CFLAGS) -c $< -o $@
+
+%.o: %.S $(HFILES) | install-headers
+	@echo " [ASM] $@"
+	$(Q)mkdir -p $(dir $@)
+	$(Q)$(call make-depend,$<,$@,$(patsubst %.o,%.d,$@))
+	$(Q)$(ASM) $(CFLAGS) -c $< -o $@
+
+%.a: $(OBJFILES)
+	@echo " [AR] $@"
+	$(Q)mkdir -p $(dir $@)
+	$(Q)$(AR) r $@ $(OBJFILES) > /dev/null 2>&1
+
+%.bin: %.elf
+	$(call cp_file,$<,$@)
+
+%.elf: $(OBJFILES)
+	@echo " [LINK] $@"
+	$(Q)mkdir -p $(dir $@)
+	$(Q)$(CC) $^ $(LDFLAGS) -o $@
+
+%.img: %.bin $(COBBLER) $(SEL4_KERNEL)
+	@echo " [IMG] $@"
+	$(Q)$(COBBLER) -k $(SEL4_KERNEL) -o $@ $<
+
+$(TARGETS): $(LIBS:%=-l%)
+
+# Avoid inadvertently passing local shared libraries with the same names as ours to the
+# linker. This is a hack and will need to be changed if we start depending on SOs.
+# (Default is .LIBPATTERNS = lib%.so lib%.a)
+.LIBPATTERNS = lib%.a
+
+DEPS = $(patsubst %.c,%.d,$(CFILES)) $(patsubst %.S,%.d,$(ASMFILES))
+
+ifneq "$(MAKECMDGOALS)" "clean"
+  -include ${DEPS}
+endif
+
+# $(call make-depend,source-file,object-file,depend-file)
+define make-depend
+  ${CC} -MM            \
+         -MF $3         \
+         -MP            \
+         -MT $2         \
+         $(CFLAGS)      \
+         $(CPPFLAGS)    \
+         $1
+endef
---
+++ files_to_obj.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -e
+
+#
+# Copyright 2014, NICTA
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(NICTA_BSD)
+#
+
+# Echo all commands if V=3; maximum verbosity.
+if [ 0${V} -ge 3 ]; then
+    set -x
+fi
+
+# Check usage.
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <output file> <symbol name> <input files> [...]"
+    echo
+    echo "Generate an '.o' file that contains the symbol 'symbol name'"
+    echo "that contains binary data of a CPIO file containing the input"
+    echo "files."
+    echo
+    exit 1
+fi
+
+# Get the absolute path of $1. Unfortunately realpath, which seems the most
+# portable option here, only works on existing files.
+touch "$1"
+OUTPUT_FILE=$(realpath "$1")
+rm "$1"
+
+SYMBOL=$2
+shift 2
+
+# Determine ".o" file to generate.
+case "$PLAT" in
+    "integratorcp"|"imx31"|"imx6"|"omap3"|"am335x"|"omap4"|\
+    "exynos4"|"exynos5"| "realview")
+        FORMAT=elf32-littlearm
+        ;;
+    "pc99")
+        FORMAT=elf32-i386
+        ;;
+    *)
+        echo "$0: Unknown platform \"$PLAT\""
+        exit 1
+        ;;
+esac
+
+# Create working directory.
+# Warning: mktemp functions differently on Linux and OSX.
+TEMP_DIR=`mktemp -d -t seL4XXXX`
+cleanup() {
+    rm -rf ${TEMP_DIR}
+}
+trap cleanup EXIT
+
+# Generate an archive of the input images.
+mkdir -p "${TEMP_DIR}/cpio"
+for i in $@; do
+    cp -f $i "${TEMP_DIR}/cpio"
+done
+pushd "${TEMP_DIR}/cpio" >/dev/null
+ARCHIVE="${TEMP_DIR}/archive.cpio"
+ls | cpio -o -H newc > ${ARCHIVE} 2>/dev/null
+popd > /dev/null
+
+# Generate a linker script.
+LINK_SCRIPT="${TEMP_DIR}/linkscript.ld"
+echo "SECTIONS { ._archive_cpio : { ${SYMBOL} = . ; *(.*) ; ${SYMBOL}_end = . ; } }" \
+        > ${LINK_SCRIPT}
+
+# Generate an output object file. We switch to the same directory as ${ARCHIVE}
+# in order to avoid symbols containing ${TEMP_DIR} polluting the namespace.
+pushd "$(dirname ${ARCHIVE})" >/dev/null
+${TOOLPREFIX}ld -T ${LINK_SCRIPT} \
+        --oformat ${FORMAT} -r -b binary $(basename ${ARCHIVE}) \
+        -o ${OUTPUT_FILE}
+popd >/dev/null
+
+# Done
+exit 0
