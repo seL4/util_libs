f92b26b9e3e4d29ee1fd426885314f56794523b4
akroh <Alexander.Kroh@nicta.com.au>
2014-08-19 13:00:06 +1000

    Exynos: Implement IRQ combiner

---
+++ src/mach/exynos/irq_combiner.c
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#include <platsupport/irq_combiner.h>
+#include "../../services.h"
+
+//#define DEBUG_COMBINER
+
+#ifdef DEBUG_MAPPINGS
+#define DCOMBINER(...) printf("Combiner:" __VA_ARGS__)
+#else
+#define DCOMBINER(...) do{}while(0)
+#endif
+
+#define NGROUPS 32
+
+struct combiner_gmap {
+    uint32_t enable_set;
+    uint32_t enable_clr;
+    uint32_t status;
+    uint32_t masked_status;
+};
+
+
+
+struct irq_combiner_map {
+    struct combiner_gmap g[NGROUPS / 4];
+    uint32_t res[32];
+    uint32_t pending;
+};
+
+volatile struct irq_combiner_map *_combiner_regs = NULL;
+
+
+#define GROUP_INDEX(cirq) (COMBINER_IRQ_GET_GROUP(cirq) >> 2)
+#define GROUP_SHIFT(cirq) (COMBINER_IRQ_GET_GROUP(cirq) & 0x3)
+#define IRQ_SHIFT(cirq)   (COMBINER_IRQ_GET_INDEX(cirq))
+#define GROUP_INDEX_MASK  0xff
+
+static volatile struct irq_combiner_map*
+irq_combiner_get_regs(irq_combiner_t* combiner) {
+    assert(combiner);
+    assert(combiner->priv);
+    return (volatile struct irq_combiner_map*)combiner->priv;
+}
+
+static int
+exynos_irq_combiner_is_pending(irq_combiner_t* combiner, combiner_irq_t cirq)
+{
+    volatile struct irq_combiner_map* regs;
+    int gidx, shift;
+    regs = irq_combiner_get_regs(combiner);
+    gidx = GROUP_INDEX(cirq);
+    shift = GROUP_SHIFT(cirq) + IRQ_SHIFT(cirq);
+    return !!(regs->g[gidx].masked_status & BIT(shift));
+}
+
+static int
+exynos_irq_combiner_is_enabled(irq_combiner_t* combiner, combiner_irq_t cirq)
+{
+    volatile struct irq_combiner_map* regs;
+    int gidx, shift;
+    regs = irq_combiner_get_regs(combiner);
+    gidx = GROUP_INDEX(cirq);
+    shift = GROUP_SHIFT(cirq) + IRQ_SHIFT(cirq);
+    return !!(regs->g[gidx].enable_set & BIT(shift));
+}
+
+static int
+exynos_irq_combiner_set_enabled(irq_combiner_t* combiner, combiner_irq_t cirq, int v)
+{
+    volatile struct irq_combiner_map* regs;
+    int gidx, shift;
+    regs = irq_combiner_get_regs(combiner);
+    gidx = GROUP_INDEX(cirq);
+    shift = GROUP_SHIFT(cirq) + IRQ_SHIFT(cirq);
+    if (v) {
+        regs->g[gidx].enable_set = BIT(shift);
+    } else {
+        regs->g[gidx].enable_clr = BIT(shift);
+    }
+    return 0;
+}
+
+static uint32_t
+exynos_irq_combiner_grp_pending(irq_combiner_t* combiner, int group)
+{
+    volatile struct irq_combiner_map* regs;
+    combiner_irq_t cirq;
+    int gidx, shift;
+    uint32_t v;
+    regs = irq_combiner_get_regs(combiner);
+    cirq = COMBINER_IRQ(group, 0);
+    gidx = GROUP_INDEX(cirq);
+    shift = GROUP_SHIFT(cirq);
+    v = regs->g[gidx].masked_status;
+    return (v >> shift) & GROUP_INDEX_MASK;
+}
+
+int
+irq_combiner_init(enum irq_combiner_id id, ps_io_ops_t* io_ops, irq_combiner_t* combiner)
+{
+    /* Map memory */
+    DCOMBINER("Mapping device ID %d\n", id);
+    switch (id) {
+    case IRQ_COMBINER0:
+        MAP_IF_NULL(io_ops, EXYNOS_IRQ_COMBINER, _combiner_regs);
+        break;
+    default:
+        return -1;
+    }
+    if (_combiner_regs == NULL) {
+        return -1;
+    }
+
+    /* Initialise structure */
+    combiner->priv = (void*)_combiner_regs;
+    combiner->is_pending  = &exynos_irq_combiner_is_pending;
+    combiner->is_enabled  = &exynos_irq_combiner_is_enabled;
+    combiner->set_enabled = &exynos_irq_combiner_set_enabled;
+    combiner->grp_pending = &exynos_irq_combiner_grp_pending;
+
+    return 0;
+}
+
+int
+irq_combiner_nirqs(enum irq_combiner_id id)
+{
+    switch (id) {
+    case IRQ_COMBINER0:
+        return 32;
+    default:
+        return -1;
+    }
+}
+
+int irq_combiner_irq(enum irq_combiner_id id, int group)
+{
+    switch (id) {
+    case IRQ_COMBINER0:
+        return group + 32;
+    default:
+        return -1;
+    }
+}
+
+
