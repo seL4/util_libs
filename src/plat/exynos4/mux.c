7be7c86b6df3e03ff7532346ba8bfa1438e3e04c
akroh <Alexander.Kroh@nicta.com.au>
2014-08-26 12:40:33 +1000

    Exynos4: Add MUX data for SPI ports

--- src/plat/exynos4/mux.c
+++ src/plat/exynos4/mux.c
@@ -58,18 +58,43 @@ static struct mux_feature_data i2c7_data[] = {
     { .port = GPIOPORT_NONE }
 };
 
+/* SPI */
+static struct mux_feature_data spi0_data[] = {
+    { .port = GPB, .pin = 0, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPB, .pin = 1, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPB, .pin = 2, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPB, .pin = 3, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPIOPORT_NONE }
+};
+static struct mux_feature_data spi1_data[] = {
+    { .port = GPB, .pin = 4, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPB, .pin = 5, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPB, .pin = 6, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPB, .pin = 7, .value = MUXVALUE_CPD(2, PUD_PULLUP, DRV3X)},
+    { .port = GPIOPORT_NONE }
+};
+static struct mux_feature_data spi2_data[] = {
+    { .port = GPC1, .pin = 1, .value = MUXVALUE_CPD(5, PUD_PULLUP, DRV3X)},
+    { .port = GPC1, .pin = 2, .value = MUXVALUE_CPD(5, PUD_PULLUP, DRV3X)},
+    { .port = GPC1, .pin = 3, .value = MUXVALUE_CPD(5, PUD_PULLUP, DRV3X)},
+    { .port = GPC1, .pin = 4, .value = MUXVALUE_CPD(5, PUD_PULLUP, DRV3X)},
+    { .port = GPIOPORT_NONE }
+};
 
 
 
 struct mux_feature_data* feature_data[] = {
-    [MUX_I2C0] = i2c0_data,
-    [MUX_I2C1] = i2c1_data,
-    [MUX_I2C2] = i2c2_data,
-    [MUX_I2C3] = i2c3_data,
-    [MUX_I2C4] = i2c4_data,
-    [MUX_I2C5] = i2c5_data,
-    [MUX_I2C6] = i2c6_data,
-    [MUX_I2C7] = i2c7_data,
+    [MUX_I2C0]     = i2c0_data,
+    [MUX_I2C1]     = i2c1_data,
+    [MUX_I2C2]     = i2c2_data,
+    [MUX_I2C3]     = i2c3_data,
+    [MUX_I2C4]     = i2c4_data,
+    [MUX_I2C5]     = i2c5_data,
+    [MUX_I2C6]     = i2c6_data,
+    [MUX_I2C7]     = i2c7_data,
+    [MUX_SPI0]     = spi0_data,
+    [MUX_SPI1]     = spi1_data,
+    [MUX_SPI2]     = spi2_data,
 };
 
 
