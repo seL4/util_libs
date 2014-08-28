1bcaea531d0f692d835a4a1c79d17041151ed706
akroh <Alexander.Kroh@nicta.com.au>
2014-08-29 01:47:33 +1000

    Add missing gpio header file for various platforms

---
+++ plat_include/am335x/platsupport/plat/gpio.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#ifndef _AM335X_GPIO_H
+#define _AM335X_GPIO_H
+
+enum gpio_port {
+    GPIO_NBANKS
+};
+
+#endif /* _AM335X_GPIO_H */
+
---
+++ plat_include/omap4/platsupport/plat/gpio.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#ifndef _OMAP4_GPIO_H
+#define _OMAP4_GPIO_H
+
+enum gpio_port {
+    GPIO_NBANKS
+};
+
+#endif /* _OMAP4_GPIO_H */
