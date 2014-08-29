c93fc568cd9c9daa6e71424dc7612d7571d91c84
akroh <Alexander.Kroh@nicta.com.au>
2014-08-29 15:22:35 +1000

    IMX6 UART: Improve driver initialisation

--- src/plat/imx6/uart.c
+++ src/plat/imx6/uart.c
@@ -8,9 +8,62 @@
  * @TAG(NICTA_BSD)
  */
 
-#include "uart.h"
 #include <stdlib.h>
 #include <platsupport/plat/uart.h>
+#include "uart.h"
+
+#define UART_PADDR  UART1_PADDR
+#define UART_REG(vaddr, x) ((volatile uint32_t*)(vaddr + (x)))
+#define UART_SR1_TRDY  13
+#define UART_SR1_RRDY  9
+/* CR1 */
+#define UART_CR1_RRDYEN 9
+#define UART_CR1_UARTEN 0
+/* CR2 */
+#define UART_CR2_SRST   0
+#define UART_CR2_RXEN   1
+#define UART_CR2_TXEN   2
+#define UART_CR2_ATEN   3
+#define UART_CR2_RTSEN  4
+#define UART_CR2_WS     5
+#define UART_CR2_STPB   6
+#define UART_CR2_PROE   7
+#define UART_CR2_PREN   8
+#define UART_CR2_RTEC   9
+#define UART_CR2_ESCEN 11
+#define UART_CR2_CTS   12
+#define UART_CR2_CTSC  13
+#define UART_CR2_IRTS  14
+#define UART_CR2_ESCI  15
+/* CR3 */
+#define UART_CR3_RXDMUXDEL  2
+/* FCR */
+#define UART_FCR_RFDIV      7
+
+#define UART_FCR_RXTL_MASK 0x1F
+  
+#define URXD  0x00 /* UART Receiver Register */
+#define UTXD  0x40 /* UART Transmitter Register */
+#define UCR1  0x80 /* UART Control Register 1 */
+#define UCR2  0x84 /* UART Control Register 2 */
+#define UCR3  0x88 /* UART Control Register 3 */
+#define UCR4  0x8c /* UART Control Register 4 */
+#define UFCR  0x90 /* UART FIFO Control Register */
+#define USR1  0x94 /* UART Status Register 1 */
+#define USR2  0x98 /* UART Status Register 2 */
+#define UESC  0x9c /* UART Escape Character Register */
+#define UTIM  0xa0 /* UART Escape Timer Register */
+#define UBIR  0xa4 /* UART BRM Incremental Register */
+#define UBMR  0xa8 /* UART BRM Modulator Register */
+#define UBRC  0xac /* UART Baud Rate Counter Register */
+#define ONEMS 0xb0 /* UART One Millisecond Register */
+#define UTS   0xb4 /* UART Test Register */
+
+#define UART_URXD_READY_MASK (1 << 15)
+#define UART_BYTE_MASK       0xFF
+#define UART_SR2_TXFIFO_EMPTY 14
+#define UART_SR2_RXFIFO_RDR    0
+
 
 #define REG_PTR(base, offset)  ((volatile uint32_t *)((char*)(base) + (offset)))
 
@@ -49,6 +102,59 @@ static void uart_handle_irq(struct ps_chardevice* d UNUSED, int irq UNUSED) {
     /* TODO */
 }
 
+#define REF_CLK 40089600
+static long set_baud(struct ps_chardevice* dev, long bps){
+    /*
+     * BaudRate = RefFreq / (16 * (BMR + 1)/(BIR + 1) )
+     * BMR and BIR are 16 bit
+     */
+    uint32_t bmr, bir;
+    bir = 0xf;
+    bmr = REF_CLK/bps - 1;
+    *REG_PTR(dev->vaddr, UBIR) = bir;
+    *REG_PTR(dev->vaddr, UBMR) = bmr;
+    return 0;
+}
+
+static int
+uart_configure(struct ps_chardevice* dev, int size, int parity, int stop) {
+    /* Character size */
+    uint32_t v;
+    v = *REG_PTR(dev->vaddr, UCR2);
+    if(size == 8){
+        v |= BIT(UART_CR2_WS);
+    }else if(size == 7){
+        v &= ~BIT(UART_CR2_WS);
+    }else{
+        return -1;
+    }
+    /* Stop bits */
+    if(stop == 2){
+        v |= BIT(UART_CR2_STPB);
+    }else if(stop == 1){
+        v &= ~BIT(UART_CR2_STPB);
+    }else{
+        return -1;
+    }
+    /* Parity */
+    if(parity == 0){
+        v &= ~BIT(UART_CR2_PREN);
+    }else if(parity == -1){
+        /* ODD */
+        v |= BIT(UART_CR2_PREN);
+        v |= BIT(UART_CR2_PROE);
+    }else if(parity == 1){
+        /* Even */
+        v |= BIT(UART_CR2_PREN);
+        v &= ~BIT(UART_CR2_PROE);
+    }else{
+        return -1;
+    }
+    /* Apply the changes */
+    *REG_PTR(dev->vaddr, UCR2) = v;
+    return 0;
+}
+
 struct ps_chardevice* uart_init(const struct dev_defn* defn,
         const ps_io_ops_t* ops,
         struct ps_chardevice* dev) {
@@ -72,12 +178,25 @@ struct ps_chardevice* uart_init(const struct dev_defn* defn,
     dev->ioops      = *ops;
     dev->clk = NULL;
 
+    /* Software reset */
+    *REG_PTR(dev->vaddr, UCR2) &= ~BIT(UART_CR2_SRST);
+    while(!(*REG_PTR(dev->vaddr, UCR2) & BIT(UART_CR2_SRST)));
+
     /* Initialise the receiver interrupt. */
-    *REG_PTR(dev->vaddr, UCR1) &= ~(1 << UART_CR1_RRDYEN);  /* Disable recv interrupt. */
+    *REG_PTR(dev->vaddr, UCR1) &= ~BIT(UART_CR1_RRDYEN);  /* Disable recv interrupt. */
     *REG_PTR(dev->vaddr, UFCR) &= ~UART_FCR_RXTL_MASK; /* Clear the rx trigger level value. */
     *REG_PTR(dev->vaddr, UFCR) |= 0x1; /* Set the rx tigger level to 1. */
-    *REG_PTR(dev->vaddr, UCR1) |= (1 << UART_CR1_RRDYEN); /* Enable recv interrupt. */
+    *REG_PTR(dev->vaddr, UCR1) |= BIT(UART_CR1_RRDYEN); /* Enable recv interrupt. */
+    *REG_PTR(dev->vaddr, UCR1) |= BIT(UART_CR1_UARTEN); /* Enable The uart. */
+    *REG_PTR(dev->vaddr, UCR2) |= BIT(UART_CR2_RXEN);  /* RX enable */
+    *REG_PTR(dev->vaddr, UCR2) |= BIT(UART_CR2_TXEN);  /* TX enable */
+    *REG_PTR(dev->vaddr, UCR2) |= BIT(UART_CR2_IRTS);  /* Ignore RTS */
+    *REG_PTR(dev->vaddr, UCR3) |= BIT(UART_CR3_RXDMUXDEL); /* RX MUX */
 
+    *REG_PTR(dev->vaddr, UFCR) &= ~(0x7 * BIT(UART_FCR_RFDIV));
+    *REG_PTR(dev->vaddr, UFCR) |= 0x4 * BIT(UART_FCR_RFDIV);
+    uart_configure(dev, 8, 0, 1);
+    set_baud(dev, 115200);
     return dev;
 }
 
