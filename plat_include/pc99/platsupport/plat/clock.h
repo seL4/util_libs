bc0050487486e26270bd57f6be437d150d6364f7
Anna Lyons <Anna.Lyons@nicta.com.au>
2014-11-05 11:16:36 +1100

    add clock.h for pc99 for consistent includes across platforms

---
+++ plat_include/pc99/platsupport/plat/clock.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#ifndef _PLATSUPPORT_PC99_CLOCK_H
+#define _PLATSUPPORT_PC99_CLOCK_H
+
+/* place holder */
+ 
+#endif /* _PLATSUPPORT_PC99_CLOCK_H */
