c0bb7fd14ca49a08a7b1b30b3108ebd043d787d3
Tim Newsham <tim.newsham@gmail.com>
2015-02-15 20:43:00 -1000

    - rework timer and serial code for am335x

---
+++ plat_include/am335x/platsupport/plat/clock.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#ifndef _PLATSUPPORT_PLAT_CLOCK_H_
+#define _PLATSUPPORT_PLAT_CLOCK_H_
+
+enum clk_id {
+    CLK_MASTER,
+    /* ----- */
+    NCLOCKS,
+    /* Custom clock */
+    CLK_CUSTOM,
+};
+
+enum clock_gate {
+    NCLKGATES
+};
+
+
+#endif /* _PLATSUPPORT_PLAT_CLOCK_H_ */
