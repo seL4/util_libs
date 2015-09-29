bc2de2d1706a87822b7bddf6975fd49f0fb8f07b
Anna Lyons <Anna.Lyons@nicta.com.au>
2015-09-29 16:41:17 +1000

    move sel4_supported_page_sizes to utils from sel4utils, add valid_size_bits function for checking page sizes are correct

---
+++ arch_include/x86/utils/arch/mapping.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+#ifndef _ARCH_MAPPING_H
+#define _ARCH_MAPPING_H
+
+#include <autoconf.h>
+
+/* ordered list of page sizes for this architecture */
+static UNUSED size_t utils_page_sizes[] = {
+    12, 
+    22, 
+};
+
+#endif /* _ARCH_MAPPING_H */
