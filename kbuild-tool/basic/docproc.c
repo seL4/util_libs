4e32606b0eb49168dca28fde0f004c3ca4f38fbd
Joel Beeren <joel.beeren@nicta.com.au>
2015-10-22 14:40:37 +1100

    kbuild-tool: expanded license headers

--- kbuild-tool/basic/docproc.c
+++ kbuild-tool/basic/docproc.c
@@ -1,4 +1,12 @@
-/* @LICENSE(KBUILD_GPL) */
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(NICTA_GPL)
+ */
 
 /*
  *	docproc is a simple preprocessor for the template files
--- kbuild-tool/kconfig/streamline_config.pl
+++ kbuild-tool/kconfig/streamline_config.pl
@@ -1,6 +1,14 @@
 #!/usr/bin/perl -w
 
-# @LICENSE(KBUILD_GPL)
+#
+# Copyright 2014, NICTA
+#
+# This software may be distributed and modified according to the terms of
+# the GNU General Public License version 2. Note that NO WARRANTY is provided.
+# See "LICENSE_GPLv2.txt" for details.
+#
+# @TAG(NICTA_GPL)
+#
 
 #
 # Copywrite 2005-2009 - Steven Rostedt
