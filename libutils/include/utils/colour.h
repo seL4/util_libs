35c5eb7fea3433ec06134ebff2634d8fa6103ca4
Stephen Sherratt <Stephen.Sherratt@nicta.com.au>
2015-12-21 14:27:56 +1100

    Using a union to switch between struct and int representations of colour

--- libutils/include/utils/colour.h
+++ libutils/include/utils/colour.h
@@ -12,27 +12,42 @@
 #define _UTILS_COLOUR_H
 
 #include <stdint.h>
+#include <byteswap.h>
+#include <endian.h>
 
-typedef struct colour_rgb24 {
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
+typedef union colour_rgb24 {
+    struct {
+        uint8_t __padding;
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+    } as_rgb;
+    uint32_t as_u32;
 } colour_rgb24_t;
 
 static inline colour_rgb24_t colour_rgb24_create(uint8_t red, uint8_t green, uint8_t blue) {
-    return (colour_rgb24_t) {red, green, blue};
+    return (colour_rgb24_t) { .as_rgb = {
+        .__padding = 0,
+        .red = red,
+        .green = green,
+        .blue = blue
+    }};
 }
 
 static inline colour_rgb24_t colour_rgb24_decode_u32(uint32_t colour) {
-    return (colour_rgb24_t) {
-        (colour >> 16) & 0xff,
-        (colour >> 8) & 0xff,
-        colour & 0xff
-    };
+#if __BYTE_ORDER == __BIG_ENDIAN
+    return (colour_rgb24_t) { .as_u32 = colour };
+#else
+    return (colour_rgb24_t) { .as_u32 = __bswap_32(colour) };
+#endif
 }
 
 static inline uint32_t colour_rgb24_encode_u32(colour_rgb24_t colour) {
-    return (colour.red << 16) | (colour.green << 8) | colour.blue;
+#if __BYTE_ORDER == __BIG_ENDIAN
+    return colour.as_u32;
+#else
+    return __bswap_32(colour.as_u32);
+#endif
 }
 
 #endif
