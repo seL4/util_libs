872f4862448eb6b622012330e23c9639133eba79
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-03-05 00:06:17 +1100

    libutils: Fix double expansion of expression within macro.
    A macro designed to only expand its arguments once is decidedly unhelpful when
    it doesn't do such.

--- libutils/include/utils/arith.h
+++ libutils/include/utils/arith.h
@@ -57,7 +57,7 @@
 #define ROUND_UP(n, b) \
     ({ typeof (n) _n = (n); \
        typeof (b) _b = (b); \
-       (_n + (_n % b == 0 ? 0 : (_b - (_n % _b)))); \
+       (_n + (_n % _b == 0 ? 0 : (_b - (_n % _b)))); \
     })
 
 #define MIN(a,b) \
