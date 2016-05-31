755a5c6fd82ff8cc29f5c5b11a3f16d2e8047df7
Yanyan Shen <yanyan.shen@nicta.com.au>
2016-05-31 22:11:19 +1000

    libplatsupport/tk1: add dummy functions

---
+++ libplatsupport/src/plat/tk1/clock.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2016, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+
+#include <stdio.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <utils/util.h>
+
+#include <platsupport/clock.h>
+#include <platsupport/plat/clock.h>
+
+
+int
+clock_sys_init(ps_io_ops_t* o, clock_sys_t* clock_sys)
+{
+    return 0;
+}
---
+++ libplatsupport/src/plat/tk1/mux.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+
+#include <stdio.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <utils/util.h>
+
+#include <platsupport/mux.h>
+#include <platsupport/gpio.h>
+
+int
+mux_sys_init(ps_io_ops_t* io_ops, mux_sys_t* mux)
+{
+    return 0;
+}
