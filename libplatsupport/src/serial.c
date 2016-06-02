b5edf8a28b83c615477363a886aa6c7c86dd2dc3
amrzar <azarrabi@nicta.com.au>
2016-06-02 12:07:38 +1000

    libplatsupport: Refactor read/write functions to serial port

---
+++ libplatsupport/src/serial.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <platsupport/serial.h>
+#include "chardev.h"
+
+ssize_t
+uart_write(ps_chardevice_t* d, const void* vdata, size_t count, chardev_callback_t rcb UNUSED, void* token UNUSED)
+{
+    const char* data = (const char*)vdata;
+    int i;
+    for (i = 0; i < count; i++) {
+        if (uart_putchar(d, data[i]) < 0) {
+            return i;
+        }
+    }
+    return count;
+}
+
+ssize_t
+uart_read(ps_chardevice_t* d, void* vdata, size_t count, chardev_callback_t rcb UNUSED, void* token UNUSED)
+{
+    char* data;
+    int ret;
+    int i;
+    data = (char*) vdata;
+    for (i = 0; i < count; i++) {
+        ret = uart_getchar(d);
+        if (ret != EOF) {
+            data[i] = ret;
+        } else {
+            return i;
+        }
+    }
+    return count;
+}
