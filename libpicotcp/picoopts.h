d50274dc2e8b5ddf5f1df5fffd81a99e68a80bd8
Addo Wondo <addo.wondo@data61.csiro.au>
2016-11-30 10:46:11 +1100

    Add libpicotcp which wraps around picotcp.
    To use libpicotcp, the source files must be linked from the picotcp source repo.

---
+++ libpicotcp/picoopts.h
@@ -0,0 +1,18 @@
+/* PicoTCP - Definition file to add support for modules */
+#pragma once
+
+#define SEL4
+
+#define PICO_SUPPORT_ETH
+#define PICO_SUPPORT_IPV4
+#define PICO_SUPPORT_UDP
+#define PICO_SUPPORT_PING
+#define PICO_SUPPORT_ICMP4
+#define PICO_SUPPORT_TCP
+#define PICO_SOCKET_TCP
+#define PICO_SUPPORT_TAP
+#define PICO_SUPPORT_MOCK
+#define DPICO_SUPPORT_DNS_CLIENT
+
+#define DEBUG_ROUTE
+#define DEBUG_ARP
