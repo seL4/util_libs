d185d574db8332d83da01f3799d9ef53bebba80e
Andrew Gacek <andrew.gacek@gmail.com>
2016-12-06 19:37:58 -0600

    Update license from NICTA to Data 61

--- elfloader-tool/src/arch-arm/plat-zynq7000/sys_fputc.h
+++ elfloader-tool/src/arch-arm/plat-zynq7000/sys_fputc.h
@@ -1,11 +1,13 @@
 /*
- * Copyright 2014, NICTA
+ * Copyright 2016, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
  *
  * This software may be distributed and modified according to the terms of
  * the GNU General Public License version 2. Note that NO WARRANTY is provided.
  * See "LICENSE_GPLv2.txt" for details.
  *
- * @TAG(NICTA_GPL)
+ * @TAG(D61_GPL)
  */
 
 #ifndef _SYS_FPUTC_H_
