6edc3e081e44dc2aef07d948888a51f91d7a7b7c
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2017-03-17 10:42:35 +1100

    utils: More detailed comment about x86 sp align

--- libutils/arch_include/x86/utils/arch/stack.h
+++ libutils/arch_include/x86/utils/arch/stack.h
@@ -12,7 +12,10 @@
 
 #pragma once
 
-/* Number of bytes to which the stack pointer must be aligned when
- * executing a call instruction
+/* The compiler will assume, and maintain, the following alignment (in bytes)
+ * of the stack pointer when a function is called. When writing assembly, or
+ * code that operates on the stack pointer directly (e.g. starting a thread),
+ * it's necessary to ensure this alignment when calling a function, so that
+ * the compiler's assumptions are correct.
  */
 #define STACK_CALL_ALIGNMENT  16
