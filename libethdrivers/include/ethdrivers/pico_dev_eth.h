f79968df5a390f8c671db8945d1cd42a970cd751
Addo Wondo <addo.wondo@data61.csiro.au>
2017-06-23 15:04:29 +1000

    Revert "Revert commits from picotcp"
    This reverts commit ba4824bb8ca6fc42645d1dff9f323206177478f1.
    
    Put in guards to prevent compilation issues when library is not in use.

--- libethdrivers/include/ethdrivers/pico_dev_eth.h
+++ libethdrivers/include/ethdrivers/pico_dev_eth.h
@@ -1,18 +1,28 @@
 /*
- * Copyright 2016, NICTA
+ *  Copyright 2017, Data61
+ *  Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ *  ABN 41 687 119 230.
  *
  * This software may be distributed and modified according to the terms of
  * the BSD 2-Clause license. Note that NO WARRANTY is provided.
  * See "LICENSE_BSD2.txt" for details.
  *
- * @TAG(NICTA_BSD)
+ * @TAG(DATA61_BSD)
  */
 
- #include <platsupport/io.h>
- #include <ethdrivers/raw.h>
- #include <ethdrivers/helpers.h>
- #include <pico_device.h>
- #include <stdint.h>
+#pragma once
+
+#ifdef CONFIG_LIB_PICOTCP
+
+#include <platsupport/io.h>
+#include <ethdrivers/raw.h>
+#include <ethdrivers/helpers.h>
+#include <pico_device.h>
+#include <stdint.h>
+
+#include <pico_stack.h>
+#include <pico_device.h>
+
 
 typedef struct pico_device_eth {
     // Pico device, wrapped inside this eth device
@@ -25,22 +35,28 @@ typedef struct pico_device_eth {
     ps_dma_man_t dma_man;
     int num_free_bufs;
     dma_addr_t **bufs;
+    dma_addr_t * dma_bufs; 
 
-    // Pico buffer
-    int num_free_rx_bufs;
-    dma_addr_t **rx_bufs;
+    int next_free_buf;
+    int *buf_pool;
+    int *rx_queue;
     int *rx_lens;
-                                                      
+    int rx_count;    
+                                                 
 } pico_device_eth;
 
- /**
-  * Creates a pico device and initialises the ethernet driver.
-  */
+/*
+ * Creates a pico device and initialises the ethernet driver.
+ */
 struct pico_device *pico_eth_create(char *name, ethif_driver_init driver_init, void *driver_config, ps_io_ops_t io_ops);
 
-/* Wrapper function for an LWIP driver for asking the underlying
+struct pico_device *pico_eth_create_no_malloc(char *name, ethif_driver_init driver_init, void *driver_config, ps_io_ops_t io_ops, pico_device_eth *pico_dev);
+
+/* Wrapper function for a picotcp driver for asking the underlying
  * eth driver to handle an IRQ */
 static inline void ethif_pico_handle_irq(pico_device_eth *iface, int irq) {
     iface->driver.i_fn.raw_handleIRQ(&iface->driver, irq);
 }
+
+#endif // CONFIG_LIB_PICOTCP
 
--- libethdrivers/src/pico_dev_eth.c
+++ libethdrivers/src/pico_dev_eth.c
@@ -1,11 +1,13 @@
 /*
- * Copyright 2016, NICTA
+ *  Copyright 2017, Data61
+ *  Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ *  ABN 41 687 119 230.
  *
  * This software may be distributed and modified according to the terms of
  * the BSD 2-Clause license. Note that NO WARRANTY is provided.
  * See "LICENSE_BSD2.txt" for details.
  *
- * @TAG(NICTA_BSD)
+ * @TAG(DATA61_BSD)
  */
 
 #include <autoconf.h>
@@ -16,175 +18,213 @@
 #include <ethdrivers/helpers.h>
 #include <string.h>
 #include "debug.h"
+#include <utils/zf_log.h>
 
-#include <pico_stack.h>
-#include <pico_device.h>
+/* Toggle for async driver, which will use eth_dsr instead of eth_poll */
+#define ASYNC_DRIVER 1
 
-static void initialize_free_bufs(pico_device_eth *pico_iface){
-    dma_addr_t *dma_bufs = NULL;
-    dma_bufs = malloc(sizeof(dma_addr_t) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
-    if(!dma_bufs){
-        goto error;
-    }
-    memset(dma_bufs, 0, sizeof(dma_addr_t) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
-    
-    pico_iface->bufs = malloc(sizeof(dma_addr_t*) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
-    if(!pico_iface->bufs){
-        goto error;
+static int alloc_buf_pool(pico_device_eth *pico_iface) {
+    /* Take the next free buffer */
+    if (pico_iface->next_free_buf == -1) {
+        ZF_LOGE("Out of preallocated eth buffers.");
+        return -1;
     }
 
-    for (int i=0; i<CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS; i++){
-        dma_bufs[i] = dma_alloc_pin(&pico_iface->dma_man, 
-			CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE, 1,  pico_iface->driver.dma_alignment);
-        if(!dma_bufs[i].phys){
-            goto error;
-        }
-        ps_dma_cache_clean_invalidate(&pico_iface->dma_man, dma_bufs[i].virt, 
-			CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
-        pico_iface->bufs[i] = &dma_bufs[i]; 
+    int retval = pico_iface->next_free_buf;
+    pico_iface->next_free_buf = pico_iface->buf_pool[retval];
+    return retval;
+
+}
+
+static void free_buf_pool(pico_device_eth *pico_iface, int buf_no) {
+    /* Return back into the buffer pool */
+    if (buf_no < 0 || buf_no > CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS) {
+        ZF_LOGE("Attempted to return a buffer outside of the pool %d.", buf_no);
+        return;
     }
-    pico_iface->num_free_bufs = CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS;
-    dma_addr_t *rx_dma_bufs = NULL;
-    rx_dma_bufs = malloc(sizeof(dma_addr_t) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
+    pico_iface->buf_pool[pico_iface->next_free_buf] = pico_iface->next_free_buf;
+    pico_iface->next_free_buf = buf_no;
+}
 
-
-    if(!rx_dma_bufs){
-        goto error;
-    }
-    memset(rx_dma_bufs, 0, sizeof(dma_addr_t) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
-    pico_iface->rx_bufs = malloc(sizeof(dma_addr_t*) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
-    if(!pico_iface->rx_bufs){
-        goto error;
-    }
-
-    /* Pin buffers */
-    for (int i=0; i<CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS; i++){
-        rx_dma_bufs[i] = dma_alloc_pin(&pico_iface->dma_man, CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE, 1,  
-                pico_iface->driver.dma_alignment);
-        if(!rx_dma_bufs[i].phys){
-            goto error;
-        }
-        ps_dma_cache_clean_invalidate(&pico_iface->dma_man, rx_dma_bufs[i].virt, CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
-        pico_iface->rx_bufs[i] = &rx_dma_bufs[i]; 
-    }
-
-    pico_iface->num_free_rx_bufs = CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS;
-    pico_iface->rx_lens = malloc(sizeof(int) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
-    if(!pico_iface->rx_lens){
-        goto error;
-    }
-    memset(pico_iface->rx_lens, 0, sizeof(int) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
-
-    return;
-
-error:
+static void destroy_free_bufs(pico_device_eth *pico_iface) {
     if (pico_iface->bufs) {
         free(pico_iface->bufs);
     }
-    if (dma_bufs) {
+    if (pico_iface->dma_bufs) {
         for (int i = 0; i < CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS; i++) {
-            if (dma_bufs[i].virt) {
-                dma_unpin_free(&pico_iface->dma_man, dma_bufs[i].virt, 
-					CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
+            if (pico_iface->dma_bufs[i].virt) {
+                dma_unpin_free(&pico_iface->dma_man, pico_iface->dma_bufs[i].virt,
+                               CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
             }
         }
-        free(dma_bufs);
+        free(pico_iface->dma_bufs);
+	pico_iface->dma_bufs = NULL;
     }
 
-    if (rx_dma_bufs) {
-        for (int i = 0; i < CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS; i++) {
-            if (rx_dma_bufs[i].virt) {
-                dma_unpin_free(&pico_iface->dma_man, rx_dma_bufs[i].virt, 
-					CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
-            }
-        }
-        free(rx_dma_bufs);
+    if (pico_iface->buf_pool) {
+        free(pico_iface->buf_pool);
     }
+    pico_iface->next_free_buf = -1;
+
+    if (pico_iface->rx_lens) {
+        free(pico_iface->rx_lens);
+    }
+
     pico_iface->bufs = NULL;
-    pico_iface->rx_bufs = NULL;
 }
 
-static uintptr_t pico_allocate_rx_buf(void *iface, size_t buf_size, void **cookie){
+static void initialize_free_bufs(pico_device_eth *pico_iface) {
+    dma_addr_t *dma_bufs = NULL;
+    dma_bufs = calloc(CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS, sizeof(dma_addr_t));
+    pico_iface->dma_bufs = dma_bufs;
+    if(!dma_bufs) {
+        destroy_free_bufs(pico_iface);
+        return;
+    }
 
+    pico_iface->bufs = malloc(sizeof(dma_addr_t*) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
+    if(!pico_iface->bufs) {
+        destroy_free_bufs(pico_iface);
+        return;
+    }
+
+    /* Pin buffers */
+    for (int i=0; i<CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS; i++) {
+        dma_bufs[i] = dma_alloc_pin(&pico_iface->dma_man,
+                                    CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE, 1,  pico_iface->driver.dma_alignment);
+        if(!dma_bufs[i].phys) {
+            destroy_free_bufs(pico_iface);
+            return;
+        }
+        ps_dma_cache_clean_invalidate(&pico_iface->dma_man, dma_bufs[i].virt,
+                                      CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
+        pico_iface->bufs[i] = &dma_bufs[i];
+    }
+
+    pico_iface->num_free_bufs = CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS;
+
+    /* Setup the buffer pool management */
+    pico_iface->buf_pool = malloc(sizeof(int) * CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS);
+    if (!pico_iface->buf_pool) {
+        destroy_free_bufs(pico_iface);
+        return;
+    }
+
+    for (int i=0; i<CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS; i++) {
+        pico_iface->buf_pool[i] = i-1;
+    }
+    pico_iface->next_free_buf = CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS - 1;
+
+    /* Rx queue */
+    pico_iface->rx_count = 0;
+    pico_iface->rx_lens = calloc(CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS, sizeof(int));
+    if(!pico_iface->rx_lens) {
+        destroy_free_bufs(pico_iface);
+        return;
+    }
+
+    pico_iface->rx_queue = calloc(CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS, sizeof(int));
+    if(!pico_iface->rx_queue) {
+        destroy_free_bufs(pico_iface);
+        return;
+    }
+
+    return;
+
+}
+
+static uintptr_t pico_allocate_rx_buf(void *iface, size_t buf_size, void **cookie) {
     pico_device_eth *pico_iface = (pico_device_eth*)iface;
 
     if (buf_size > CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE) {
-        LOG_ERROR("Requested RX buffer of size %d which can never be fullfilled by preallocated buffers of size %d", buf_size, CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
+        ZF_LOGE("Requested RX buffer of size %d which can never be fullfilled by preallocated buffers of size %d",
+                buf_size, CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE);
         return 0;
     }
 
-    if (pico_iface->num_free_rx_bufs == 0) {
-        if (!pico_iface->rx_bufs) {
-            initialize_free_bufs(pico_iface);
-            if (!pico_iface->rx_bufs) {
-                LOG_ERROR("Failed lazy initialization of preallocated free buffers");
-                return 0;
-            }
-        } else {
+    if (!pico_iface->bufs) {
+        initialize_free_bufs(pico_iface);
+        if (!pico_iface->bufs) {
+            ZF_LOGE("Failed lazy initialization of preallocated free buffers");
             return 0;
         }
     }
-    pico_iface->num_free_rx_bufs--;
-    dma_addr_t *buf = pico_iface->rx_bufs[pico_iface->num_free_rx_bufs];
+
+    int buf_no = alloc_buf_pool(pico_iface);
+    if (buf_no < 0) {
+        /* No buffers available */
+        return 0;
+    }
+
+    dma_addr_t *buf = pico_iface->bufs[buf_no];
     ps_dma_cache_invalidate(&pico_iface->dma_man, buf->virt, buf_size);
-
-    /* Also store the information about the length */
-    pico_iface->rx_lens[pico_iface->num_free_rx_bufs] = buf_size;
-
-    *cookie = (void*)buf;
+    *cookie = buf_no;
     return buf->phys;
-
-
 }
 
 static void pico_tx_complete(void *iface, void *cookie) {
     pico_device_eth *pico_iface = (pico_device_eth*)iface;
-    pico_iface->bufs[pico_iface->num_free_bufs] = cookie;
-    pico_iface->num_free_bufs++;
-}
-
-static void clear_rx_buf(void *iface, void *cookie) {
-    pico_device_eth *pico_iface = (pico_device_eth*)iface;
-    pico_iface->rx_bufs[pico_iface->num_free_rx_bufs] = cookie;
-    pico_iface->num_free_rx_bufs++;
+    free_buf_pool(iface, (int)cookie);
 }
 
 static void pico_rx_complete(void *iface, unsigned int num_bufs, void **cookies, unsigned int *lens) {
+    /* A buffer has been filled. Put it into the receive queue to be collected. */
+    pico_device_eth *pico_iface = (pico_device_eth*)iface;
 
-    // Do nothing. The data has been injected into the rx bufs, for picoTCP to collect. 
-    // Let pico tick do the work.
-    //printf("RX complete %d left!\n", ((pico_device_eth*)iface)->num_free_rx_bufs);
+    if (num_bufs > 1) {
+        ZF_LOGE("RX buffer of size is smaller than MTU. Frame splitting unhandled.\n");
+        /* Frame splitting is not handled. Warn and return bufs to pool. */
+        for (int i=0; i<num_bufs; i++) {
+            free_buf_pool(pico_iface, cookies[i]);
+        }
+    } else {
+        int buf_no = cookies[0];
+        /* Store the information about the rx bufs */
+        pico_iface->rx_queue[pico_iface->rx_count] = buf_no;
+        pico_iface->rx_lens[buf_no] = lens[0];
+        pico_iface->rx_count += 1;
+
+        if(ASYNC_DRIVER) {
+            pico_iface->pico_dev.__serving_interrupt = 1;
+        }
+    }
+    ZF_LOGD("RX complete, %d in queue!\n", ((pico_device_eth*)iface)->rx_count);
 
     return;
 }
 
 /* Pico TCP implementation */
 
-static int pico_eth_send(struct pico_device *dev, void *input_buf, int len){
+static int pico_eth_send(struct pico_device *dev, void *input_buf, int len) {
 
     dma_addr_t buf;
     int status;
     struct pico_device_eth *eth_device = (struct pico_device_eth *)dev;
-    
-    if (len > CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE || eth_device->num_free_bufs == 0){
+
+    if (len > CONFIG_LIB_ETHDRIVER_PREALLOCATED_BUF_SIZE) {
         return 0;
     }
-    eth_device->num_free_bufs--;
-    dma_addr_t *orig_buf = eth_device->bufs[eth_device->num_free_bufs];
+
+    int buf_no = alloc_buf_pool(eth_device);
+    if (buf_no < 0) {
+        return 0;
+    }
+
+    dma_addr_t *orig_buf = eth_device->bufs[buf_no];
     buf = *orig_buf;
     memcpy(buf.virt, input_buf, len);
     ps_dma_cache_clean(&eth_device->dma_man, buf.virt, len);
-    
+
     unsigned int length = len;
-    status = eth_device->driver.i_fn.raw_tx(&eth_device->driver, 1, &buf.phys, &length, orig_buf);
+    status = eth_device->driver.i_fn.raw_tx(&eth_device->driver, 1, &buf.phys, &length, buf_no);
 
     switch(status) {
     case ETHIF_TX_FAILED:
-        pico_tx_complete(dev, orig_buf);
-        LOG_ERROR("Failed tx\n");
+        pico_tx_complete(dev, buf_no);
+        ZF_LOGE("Failed tx\n");
         return 0; // Error for PICO
     case ETHIF_TX_COMPLETE:
-        pico_tx_complete(dev, orig_buf);
+        pico_tx_complete(dev, buf_no);
     case ETHIF_TX_ENQUEUED:
         break;
     }
@@ -193,28 +233,32 @@ static int pico_eth_send(struct pico_device *dev, void *input_buf, int len){
 
 }
 
-static int pico_eth_poll(struct pico_device *dev, int loop_score){
+/* Async driver */
+static int pico_eth_poll(struct pico_device *dev, int loop_score) {
     struct pico_device_eth *eth_device = (struct pico_device_eth *)dev;
-    while (loop_score > 0){
-        if (eth_device->num_free_rx_bufs == CONFIG_LIB_ETHDRIVER_NUM_PREALLOCATED_BUFFERS){
+    while (loop_score > 0) {
+        if (eth_device->rx_count == 0) {
+            if (ASYNC_DRIVER) {
+                /* Also clear the serving_interrupt flag for async driver*/
+                eth_device->pico_dev.__serving_interrupt = 0;
+            }
             break;
         }
 
         /* Retrieve the data from the rx buffer */
-        dma_addr_t *buf = eth_device->rx_bufs[eth_device->num_free_rx_bufs];
+        eth_device->rx_count -= 1;
+        int buf_no = eth_device->rx_queue[eth_device->rx_count];
+        dma_addr_t *buf = eth_device->bufs[buf_no];
 
-        /* Retrieve the length of the data in the frame */
-        int len = eth_device->rx_lens[eth_device->num_free_rx_bufs];
-
+        int len = eth_device->rx_lens[buf_no];
         ps_dma_cache_invalidate(&eth_device->dma_man, buf->virt, len);
         pico_stack_recv(dev, buf->virt, len);
 
-        eth_device->num_free_rx_bufs++;
+        free_buf_pool(eth_device, buf_no);
         loop_score--;
-
     }
-    return loop_score;
 
+    return loop_score;
 }
 
 static struct raw_iface_callbacks pico_prealloc_callbacks = {
@@ -223,13 +267,11 @@ static struct raw_iface_callbacks pico_prealloc_callbacks = {
     .allocate_rx_buf = pico_allocate_rx_buf
 };
 
-struct pico_device *pico_eth_create(char *name, 
-    ethif_driver_init driver_init, void *driver_config, ps_io_ops_t io_ops){
+struct pico_device *pico_eth_create_no_malloc(char *name,
+        ethif_driver_init driver_init, void *driver_config, ps_io_ops_t io_ops, struct pico_device_eth *eth_dev) {
 
-    /* Create the pico device struct */
-    struct pico_device_eth *eth_dev = malloc(sizeof(struct pico_device_eth));
-    if (!eth_dev){
-        LOG_ERROR("Failed to malloc pico eth device interface");
+    if (eth_dev == NULL) {
+        ZF_LOGE("Invalid pico_device passed into Pico create no_malloc");
         return NULL;
     }
 
@@ -240,26 +282,31 @@ struct pico_device *pico_eth_create(char *name,
     eth_dev->dma_man = io_ops.dma_manager;
 
     eth_dev->driver.cb_cookie = eth_dev;
-
     /* Initialize hardware */
     int err;
     err = driver_init(&(eth_dev->driver), io_ops, driver_config);
-    if (err){ 
+    if (err) {
         return NULL;
     }
 
     /* Initialise buffers in case driver did not do so */
-    if (!eth_dev->bufs){
+    if (!eth_dev->bufs) {
         initialize_free_bufs(eth_dev);
     }
 
     /* Attach funciton pointers */
     eth_dev->pico_dev.send = pico_eth_send;
-    eth_dev->pico_dev.poll = pico_eth_poll;
+    if (ASYNC_DRIVER) {
+        /* Although the same function, .poll needs to be set to NULL for an async driver */
+        eth_dev->pico_dev.poll = NULL;
+        eth_dev->pico_dev.dsr = pico_eth_poll;
+    } else {
+        eth_dev->pico_dev.poll = pico_eth_poll;
+    }
 
     /* Also do some low level init */
     int mtu;
-    uint8_t mac[6] = {};
+    uint8_t mac[6] = {0};
 
     eth_dev->driver.i_fn.low_level_init(&eth_dev->driver, mac, &mtu);
 
@@ -267,8 +314,8 @@ struct pico_device *pico_eth_create(char *name,
     eth_dev->pico_dev.mtu = mtu;
 
     /* Register in picoTCP (equivalent to netif init in lwip)*/
-    if (pico_device_init(&(eth_dev->pico_dev), name, mac) != 0){
-        LOG_ERROR("Failed to initialize pico device");
+    if (pico_device_init(&(eth_dev->pico_dev), name, mac) != 0) {
+        ZF_LOGE("Failed to initialize pico device");
         free(eth_dev);
         return NULL;
     }
@@ -276,5 +323,19 @@ struct pico_device *pico_eth_create(char *name,
     return (struct pico_device *)eth_dev;
 }
 
-#endif /* CONFIG_LIB_PICOTCP */
+
+struct pico_device *pico_eth_create(char *name,
+                                    ethif_driver_init driver_init, void *driver_config, ps_io_ops_t io_ops) {
+
+    /* Create the pico device struct */
+    struct pico_device_eth *eth_dev = malloc(sizeof(struct pico_device_eth));
+    if (!eth_dev) {
+        ZF_LOGE("Failed to malloc pico eth device interface");
+        return NULL;
+    }
+
+    return pico_eth_create_no_malloc(name, driver_init, driver_config, io_ops, eth_dev);
+}
+
+#endif // CONFIG_LIB_PICOTCP
 
