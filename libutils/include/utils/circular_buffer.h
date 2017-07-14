277d068be846716990cc4876fad77045665b9a71
Siwei Zhuang <siwei.zhuang@data61.csiro.au>
2017-07-14 16:46:36 +1000

    libutils: Added circular buffer implementation.

---
+++ libutils/include/utils/circular_buffer.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+/**
+ * @file circular_buffer.h
+ * @brief A circular buffer implementation
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <sys/types.h>
+
+/* Circular Buffer */
+typedef struct circ_buf {
+	uint8_t *buf;
+	off_t head;
+	off_t tail;
+	size_t size;
+} circ_buf_t;
+
+/**
+ * Create a new circular buffer
+ *
+ * @param base A pointer to the start of the region to use as the buffer.
+ * @param size The size of the buffer in bytes.
+ * @param[in] cb Circular buffer structure allocated by the user.
+ *
+ * @return NULL on failure.
+ */
+int circ_buf_new(void *base, size_t size, circ_buf_t *cb);
+
+/**
+ * Destroy a circular buffer
+ *
+ * @param cb Circular buffer to Destroy.
+ */
+void circ_buf_free(circ_buf_t *cb);
+
+/**
+ * Check if the circular buffer is full
+ *
+ * @param cb Circular buffer to check
+ *
+ * @return true indicates the buffer is full, false otherwise.
+ */
+bool circ_buf_is_full(circ_buf_t *cb);
+
+/**
+ * Check if the circular buffer is empty
+ *
+ * @param cb Circular buffer to check
+ *
+ * @return true indicates the buffer is empty, false otherwise.
+ */
+bool circ_buf_is_empty(circ_buf_t *cb);
+
+/**
+ * Put a byte
+ *
+ * @param cb Circular buffer to put via.
+ * @param c  Byte to send.
+ */
+void circ_buf_put(circ_buf_t *cb, uint8_t c);
+
+/**
+ * Get a byte
+ *
+ * @param cb Circular buffer to get from.
+ * 
+ * @return The byte received.
+ */
+uint8_t circ_buf_get(circ_buf_t *cb);
+
---
+++ libutils/src/circular_buffer.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <utils/zf_log.h>
+#include <utils/circular_buffer.h>
+
+static inline off_t _next_pos(circ_buf_t *cb, off_t pos)
+{
+	return (pos + 1) % cb->size;
+}
+
+int circ_buf_new(void *base, size_t size, circ_buf_t *cb)
+{
+	if (!base || size == 0 || !cb) {
+		ZF_LOGE("Invalid arguments\n");
+		return EINVAL;
+	}
+
+	cb->buf = (uint8_t*)base;
+	cb->head = 0;
+	cb->tail = 0;
+	cb->size = size;
+
+	return cb;
+}
+
+void circ_buf_free(circ_buf_t *cb)
+{
+	if (cb) {
+		cb->buf = NULL;
+		cb->head = 0;
+		cb->tail = 0;
+		cb->size = 0;
+		free(cb);
+	} else {
+		ZF_LOGW("Freeing NULL pointer\n");
+	}
+}
+
+bool circ_buf_is_full(circ_buf_t *cb)
+{
+	return _next_pos(cb, cb->tail) == cb->head;
+}
+
+bool circ_buf_is_empty(circ_buf_t *cb)
+{
+	return cb->tail == cb->head;
+}
+
+void circ_buf_put(circ_buf_t *cb, uint8_t c)
+{
+	cb->buf[cb->tail] = c;
+	cb->tail = _next_pos(cb, cb->tail);
+}
+
+uint8_t circ_buf_get(circ_buf_t *cb)
+{
+	uint8_t c = cb->buf[cb->head];
+	cb->head = _next_pos(cb, cb->head);
+
+	return c;
+}
+
