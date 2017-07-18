09bbc6272e77bbf178ca038157fb9be86fcca9b8
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-07-18 10:49:24 +1000

    platsupport: Move sync primitives from sel4sync

---
+++ libplatsupport/include/platsupport/sync/atomic.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#ifndef _SYNC_ATOMIC_H_
+#define _SYNC_ATOMIC_H_
+
+#include <assert.h>
+#include <limits.h>
+#include <stddef.h>
+
+/** \brief Atomically increment an integer, accounting for possible overflow.
+ *
+ * @param x Pointer to integer to increment.
+ * @param[out] oldval Previous value of the integer. May be written to even if
+ *   the increment fails.
+ * @param success_memorder The memory order to enforce
+ * @return 0 if the increment succeeds, non-zero if it would cause an overflow.
+ */
+static inline int sync_atomic_increment_safe(volatile int *x, int *oldval, int success_memorder) {
+    assert(x != NULL);
+    assert(oldval != NULL);
+    do {
+        *oldval = *x;
+        if (*oldval == INT_MAX) {
+            /* We would overflow */
+            return -1;
+        }
+    } while (!__atomic_compare_exchange_n(x, oldval, *oldval + 1, 1, success_memorder, __ATOMIC_RELAXED));
+    return 0;
+}
+
+/** \brief Atomically decrement an integer, accounting for possible overflow.
+ *
+ * @param x Pointer to integer to decrement.
+ * @param[out] oldval Previous value of the integer. May be written to even if
+ *   the decrement fails.
+ * @param success_memorder The memory order to enforce if the decrement is successful
+ * @return 0 if the decrement succeeds, non-zero if it would cause an overflow.
+ */
+static inline int sync_atomic_decrement_safe(volatile int *x, int *oldval, int success_memorder) {
+    assert(x != NULL);
+    assert(oldval != NULL);
+    do {
+        *oldval = *x;
+        if (*oldval == INT_MIN) {
+            /* We would overflow */
+            return -1;
+        }
+    } while (!__atomic_compare_exchange_n(x, oldval, *oldval - 1, 1, success_memorder, __ATOMIC_RELAXED));
+    return 0;
+}
+
+/* Atomically increment an integer and return its new value. */
+static inline int sync_atomic_increment(volatile int *x, int memorder) {
+    return __atomic_add_fetch(x, 1, memorder);
+}
+
+/* Atomically decrement an integer and return its new value. */
+static inline int sync_atomic_decrement(volatile int *x, int memorder) {
+    return __atomic_sub_fetch(x, 1, memorder);
+}
+
+#endif
---
+++ libplatsupport/include/platsupport/sync/spinlock.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#ifndef _SYNC_SPINLOCK_H_
+#define _SYNC_SPINLOCK_H_
+
+/* Interface for spin locks. Note, the underlying implementation is not
+ * seL4-specific. Spin locks should not be used when you have threads of
+ * different priorities.
+ */
+
+#include <stdbool.h>
+
+typedef int sync_spinlock_t;
+
+static inline int sync_spinlock_init(sync_spinlock_t *lock) {
+    *lock = 0;
+    return 0;
+}
+
+static inline int sync_spinlock_lock(sync_spinlock_t *lock) {
+    while (true) {
+        int expected = 0;
+        if (__atomic_compare_exchange_n(lock, &expected, 1, 1, __ATOMIC_ACQUIRE, __ATOMIC_RELAXED)) {
+            break;
+        }
+    }
+    return 0;
+}
+
+static inline int sync_spinlock_trylock(sync_spinlock_t *lock) {
+    int expected = 0;
+    return !__atomic_compare_exchange_n(lock, &expected, 1, 0, __ATOMIC_ACQUIRE, __ATOMIC_RELAXED);
+}
+
+static inline int sync_spinlock_unlock(sync_spinlock_t *lock) {
+    __atomic_store_n(lock, 0, __ATOMIC_RELEASE);
+    return 0;
+}
+
+static inline int sync_spinlock_destroy(sync_spinlock_t *lock) {
+    /* Nothing required. */
+    return 0;
+}
+
+#endif
