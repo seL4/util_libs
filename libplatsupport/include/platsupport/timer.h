f5c33eb7044600457194ef15e4aef94e8f2201b8
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-07-19 16:50:35 +1000

    Remove pstimer_t -> deprecated by ltimer

--- libplatsupport/include/platsupport/timer.h
+++ libplatsupport/include/platsupport/timer.h
@@ -10,17 +10,11 @@
  * @TAG(DATA61_BSD)
  */
 
-#ifndef __PLATSUPPORT_TIMER_H__
-#define __PLATSUPPORT_TIMER_H__
+#pragma once
 
 #include <stdint.h>
-#include <errno.h>
 
-#include <platsupport/io.h>
-
-typedef struct pstimer pstimer_t;
-
-/* Properties of a timer */
+/* Properties of a single timer device */
 typedef struct {
     /* Timers are up counters or down counters.
      *
@@ -41,140 +35,6 @@ typedef struct {
     /* when does this timer roll over? This will be 0 for down-counters (max valueue 64) */
     uint32_t bit_width: 7;
 
-    /* Number of irqs this timer issues */
+    /* Number of unique irqs this timer issues */
     uint32_t irqs;
 } timer_properties_t;
-
-struct pstimer {
-    /* Properties of this timer */
-    timer_properties_t properties;
-    /*
-     * Start the timer. Timer must be initialised.
-     *
-     * @return 0 on success.
-     */
-    int  (*start)(const pstimer_t* device);
-
-    /* Stop the timer. init does not need to be called again.
-     *
-     * @return 0 on success.
-     */
-    int  (*stop)(const pstimer_t* device);
-
-    /**
-     * Get the current time since this timer last overflowed or was reset.
-     * The value returned is expected to be in nanoseconds, consistent with
-     * the rest of the API.
-     */
-    uint64_t (*get_time)(const pstimer_t* device);
-
-    /**
-     * Set an irq to come in at ns.
-     * This operation is only supported by count up timers.
-     *
-     * @param ns the absolute time in nanoseconds to fire an irq.
-     * @return 0 on success.
-     *         ENOSYS if unsupported,
-     *         ETIME if that time has already passed,
-     *         EINVAL if ns is beyond what the timer supports (can't count that high).
-     */
-    int (*oneshot_absolute)(const pstimer_t* device, uint64_t ns);
-
-    /* Set an irq for current time + ns.
-     *
-     * @param ns nanoseconds in the future to fire an irq.
-     * @return 0 on success.
-     *         ENOSYS if unsupported.
-     *         ETIME if ns is too small and a race occurs.
-     *         EINVAL if ns is too big for the timer.
-     */
-    int (*oneshot_relative)(const pstimer_t* device, uint64_t ns);
-
-    /* Set an irq to keep recurring every time ns passes.
-     *
-     * If ns is too small and causes races on any but the first irq, periodic
-     * mode will be disabled.
-     *
-     * @param ns amout of time in nanoseconds to fire a recurring irq.
-     * @return 0 on success.
-     *         ENOSYS if unsupported.
-     *         ETIME if ns is too small and a race occurs.
-     *         EINVAL if ns is too big for the timer.
-     */
-    int (*periodic)(const pstimer_t* device, uint64_t ns);
-
-    /*
-     * Handle an irq.
-     * Call this after every irq comes in.
-     *
-     * @param irq the irq number that fired.
-     */
-    void (*handle_irq)(const pstimer_t* device, uint32_t irq);
-
-    /*
-     * @return the nth irq number that this timer generates.
-     */
-    uint32_t (*get_nth_irq)(const pstimer_t *device, uint32_t n);
-
-    /* data for the timer impl */
-    void* data;
-};
-
-/* Basic wrappers for code neatness */
-static inline int
-timer_start(pstimer_t* device)
-{
-    return device->start(device);
-}
-
-static inline int
-timer_stop(pstimer_t* device)
-{
-    return device->stop(device);
-}
-
-static inline uint64_t
-timer_get_time(pstimer_t* device)
-{
-
-    return device->get_time(device);
-}
-
-static inline int
-timer_oneshot_absolute(pstimer_t* device, uint64_t ns)
-{
-    return device->oneshot_absolute(device, ns);
-}
-
-static inline int
-timer_oneshot_relative(pstimer_t* device, uint64_t ns)
-{
-    return device->oneshot_relative(device, ns);
-}
-
-static inline int
-timer_periodic(pstimer_t* device, uint64_t ns)
-{
-    return device->periodic(device, ns);
-}
-
-static inline void
-timer_handle_irq(pstimer_t* device, uint32_t irq)
-{
-    device->handle_irq(device, irq);
-}
-
-static inline uint32_t
-timer_get_nth_irq(const pstimer_t *device, uint32_t n)
-{
-    return device->get_nth_irq(device, n);
-}
-
-/**
- * Initialise a timer
- * @param  id  The ID of the timer to initialise
- * @return     A handle to the initialise timer or NULL on error
- */
-pstimer_t *ps_init_timer(int id, ps_io_ops_t* io_ops);
-
-#endif /* __PLATSUPPORT_TIMER_H__ */
