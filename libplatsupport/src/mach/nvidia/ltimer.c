12ef6aa865ec0f1c7c6500db65d72c474d1f4504
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-07-26 09:02:04 +1000

    trivial: fix licenses

--- libplatsupport/src/mach/nvidia/ltimer.c
+++ libplatsupport/src/mach/nvidia/ltimer.c
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
 /* Implementation of a logical timer for omap platforms
  *
  * We use two GPTS: one for the time and relative timeouts, the other
--- libplatsupport/src/plat/pc99/ltimer.c
+++ libplatsupport/src/plat/pc99/ltimer.c
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
 /* Implementation of a logical timer for pc99 platforms
  *
  * We try to use the HPET, but if that doesn't work we use the PIT.
