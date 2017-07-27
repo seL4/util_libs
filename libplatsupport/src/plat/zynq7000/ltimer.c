b2670005df372ca7e22be4bd7bfa18c9bda10e89
Hesham Almatary <hesham.almatary@data61.csiro.au>
2017-07-27 14:25:36 +1000

    ltimer/zynq: use ttc1_timer1 as a timestamp/gettime timer

--- libplatsupport/src/plat/zynq7000/ltimer.c
+++ libplatsupport/src/plat/zynq7000/ltimer.c
@@ -20,11 +20,18 @@
 
 #include <utils/util.h>
 
-#define TTC_ID TTC0_TIMER1
+/* Use ttc0_timer1 for timeouts/sleep */
+#define TTC_TIMEOUT   TTC0_TIMER1
+/* Use ttc1_timer1 to keep running for timestamp/gettime */
+#define TTC_TIMESTAMP TTC1_TIMER1
 
 typedef struct {
-    ttc_t ttc;
-    void *vaddr;
+    ttc_t ttc_timeout;
+    void *vaddr_timeout;
+
+    ttc_t ttc_timestamp;
+    void *vaddr_timestamp;
+
     ps_io_ops_t ops;
 } ttc_ltimer_t;
 
@@ -38,20 +45,30 @@ static int get_nth_irq(void *data, size_t n, ps_irq_t *irq)
     assert(n < get_num_irqs(data));
 
     irq->type = PS_INTERRUPT;
-    irq->irq.number = ttc_irq(TTC_ID);
+    irq->irq.number = ttc_irq(TTC_TIMEOUT);
     return 0;
 }
 
 static size_t get_num_pmems(void *data)
 {
-    return 1;
+    return 2;
 }
 
 static int get_nth_pmem(void *data, size_t n, pmem_region_t *region)
 {
     assert(n < get_num_pmems(data));
     region->length = PAGE_SIZE_4K;
-    region->base_addr = ttc_paddr(TTC_ID);
+
+    switch(n) {
+    case 0:
+        region->base_addr = ttc_paddr(TTC_TIMEOUT);
+        break;
+    case 1:
+        region->base_addr = ttc_paddr(TTC_TIMESTAMP);
+        break;
+    default:
+        ZF_LOGE("Invalid timer number");
+    }
     return 0;
 }
 
@@ -59,7 +76,8 @@ static int handle_irq(void *data, ps_irq_t *irq)
 {
     assert(data != NULL);
     ttc_ltimer_t *ttc_ltimer = data;
-    ttc_handle_irq(&ttc_ltimer->ttc);
+    /* Currently, only timeout interrupts are handled */
+    ttc_handle_irq(&ttc_ltimer->ttc_timeout);
     return EINVAL;
 }
 
@@ -68,8 +86,8 @@ static int get_time(void *data, uint64_t *time)
     assert(data != NULL);
     assert(time != NULL);
 
-    ttc_t * ttc0_timer1 = (ttc_t *) data;
-    *time = ttc_get_time(ttc0_timer1);
+    ttc_t * ttc1_timer1 = &(((ttc_ltimer_t *) data)->ttc_timestamp);
+    *time = ttc_get_time(ttc1_timer1);
 
     return 0;
 }
@@ -88,7 +106,7 @@ static int set_timeout(void *data, uint64_t ns, timeout_type_t type)
         return ENOSYS;
     }
 
-    return ttc_set_timeout(&ttc_ltimer->ttc, ns, type == TIMEOUT_PERIODIC);
+    return ttc_set_timeout(&ttc_ltimer->ttc_timeout, ns, type == TIMEOUT_PERIODIC);
 }
 
 static int reset(void *data)
@@ -97,8 +115,10 @@ static int reset(void *data)
     ttc_ltimer_t *ttc_ltimer = data;
 
     /* reset the timers */
-    ttc_stop(&ttc_ltimer->ttc);
-    ttc_start(&ttc_ltimer->ttc);
+    ttc_stop(&ttc_ltimer->ttc_timeout);
+    ttc_start(&ttc_ltimer->ttc_timeout);
+    ttc_stop(&ttc_ltimer->ttc_timestamp);
+    ttc_start(&ttc_ltimer->ttc_timestamp);
 
     return 0;
 }
@@ -108,12 +128,20 @@ static void destroy(void *data)
     assert(data);
 
     ttc_ltimer_t *ttc_ltimer = data;
-    if (ttc_ltimer->vaddr) {
-        ttc_stop(&ttc_ltimer->ttc);
+    if (ttc_ltimer->vaddr_timeout) {
+        ttc_stop(&ttc_ltimer->ttc_timeout);
         pmem_region_t region;
         UNUSED int error = get_nth_pmem(data, 0, &region);
         assert(!error);
-        ps_pmem_unmap(&ttc_ltimer->ops, region, ttc_ltimer->vaddr);
+        ps_pmem_unmap(&ttc_ltimer->ops, region, ttc_ltimer->vaddr_timeout);
+    }
+
+    if (ttc_ltimer->vaddr_timestamp) {
+        ttc_stop(&ttc_ltimer->ttc_timestamp);
+        pmem_region_t region;
+        UNUSED int error = get_nth_pmem(data, 1, &region);
+        assert(!error);
+        ps_pmem_unmap(&ttc_ltimer->ops, region, ttc_ltimer->vaddr_timestamp);
     }
 
     ps_free(&ttc_ltimer->ops.malloc_ops, sizeof(ttc_ltimer), ttc_ltimer);
@@ -141,23 +169,50 @@ int ltimer_default_init(ltimer_t *ltimer, ps_io_ops_t ops)
     ttc_ltimer_t *ttc_ltimer = ltimer->data;
     ttc_ltimer->ops = ops;
 
+    /* Default timeout timer */
     pmem_region_t region;
     error = get_nth_pmem(ltimer->data, 0, &region);
     assert(error == 0);
-    ttc_ltimer->vaddr = ps_pmem_map(&ops, region, false, PS_MEM_NORMAL);
-    if (ttc_ltimer->vaddr == NULL) {
+    ttc_ltimer->vaddr_timeout = ps_pmem_map(&ops, region, false, PS_MEM_NORMAL);
+    if (ttc_ltimer->vaddr_timeout == NULL) {
+        error = -1;
+    }
+
+    /* Default timestamp timer */
+    pmem_region_t region_timestamp;
+    error = get_nth_pmem(ltimer->data, 1, &region_timestamp);
+    assert(error == 0);
+    ttc_ltimer->vaddr_timestamp = ps_pmem_map(&ops, region_timestamp, false, PS_MEM_NORMAL);
+    if (ttc_ltimer->vaddr_timestamp == NULL) {
         error = -1;
     }
 
     ttc_config_t config = {
-        .vaddr = ttc_ltimer->vaddr,
-        .id = TTC_ID,
+        .vaddr = ttc_ltimer->vaddr_timeout,
+        .id = TTC_TIMEOUT,
     };
 
-    error = ttc_init(&ttc_ltimer->ttc ,config);
+    ttc_config_t config_timestamp = {
+        .vaddr = ttc_ltimer->vaddr_timestamp,
+        .id = TTC_TIMESTAMP,
+    };
+
+    error = ttc_init(&ttc_ltimer->ttc_timeout ,config);
+    assert(error == 0);
+    error = ttc_start(&ttc_ltimer->ttc_timeout);
+    assert(error == 0);
+
+    error = ttc_init(&ttc_ltimer->ttc_timestamp ,config_timestamp);
 
     if (!error) {
-        error = ttc_start(&ttc_ltimer->ttc);
+        /* FIXME: This is a hack to set the prescalar value of ttc1_timer1 to keep
+         * it free-running. Overflow interrupts are not handled.
+         * Ideally, there should be an appropriate overflow handler to get us an
+         * accurate 64-bit (simulated) time in nanoseconds, using one or more 16-bit
+         * ttc timers provided by zynq.
+         */
+        error = ttc_set_timeout(&ttc_ltimer->ttc_timestamp, 2 * NS_IN_S, true);
+        error |= ttc_start(&ttc_ltimer->ttc_timestamp);
     }
 
     if (error) {
