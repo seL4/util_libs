033ab78e3ea14c322194a29bd9b87b7b20755945
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-08-04 23:26:42 +1000

    libelf: Retrieve ELF constants from underlying C library
    Use the System V defined ELF extension header to retrieve the ELF format constants. Other
    parts of this library could also be changed to use definitions from the standard ELF header,
    but these are the ones that generate name collisions when using both this library and the
    ELF header concurrently.

--- libelf/include/elf/elf.h
+++ libelf/include/elf/elf.h
@@ -106,73 +106,9 @@ ELF file into memory.
 #include <stdint.h>
 #include <stdio.h>
 
+#include <elf.h>
 #include "elf32.h"
 #include "elf64.h"
-
-/*
- * constants for Elf32_Phdr.p_flags
- */
-#define PF_X		1	/* readable segment */
-#define PF_W		2	/* writeable segment */
-#define PF_R		4	/* executable segment */
-
-/*
- * constants for indexing into Elf64_Header_t.e_ident
- */
-#define EI_MAG0		0
-#define EI_MAG1		1
-#define EI_MAG2		2
-#define EI_MAG3		3
-#define EI_CLASS	4
-#define EI_DATA		5
-#define EI_VERSION	6
-
-#define ELFMAG0         '\177'
-#define ELFMAG1         'E'
-#define ELFMAG2         'L'
-#define ELFMAG3         'F'
-
-#define ELFCLASS32      1
-#define ELFCLASS64      2
-
-#define PT_NULL 0
-#define PT_LOAD 1
-#define PT_DYNAMIC 2
-#define PT_INTERP 3
-#define PT_NOTE 4
-#define PT_SHLIB 5
-#define PT_PHDR 6
-#define PT_TLS 7
-#define PT_NUM 8
-
-#define ELFDATA2LSB 1
-#define ELFDATA2MSB 2
-
-/* Section Header type bits */
-#define SHT_PROGBITS 1
-#define SHT_RELA 4
-#define SHT_SYMTAB 2
-#define	SHT_NOBITS 8
-#define SHT_REL 9
-
-/* Section Header flag bits */
-#define SHF_WRITE 1
-#define SHF_ALLOC 2
-#define SHF_EXECINSTR  4
-
-/* Dynamic section entry types */
-#define DT_NULL 0
-#define DT_HASH 4
-#define DT_STRTAB 5
-#define DT_SYMTAB 6
-#define DT_RELA 7
-#define DT_RELASZ 8
-#define DT_RELAENT 9
-#define DT_STRSZ 10
-#define DT_SYMENT 11
-#define DT_REL 17
-#define DT_RELSZ 18
-#define DT_RELENT 19
 
 /**/
 #define ELF_PRINT_PROGRAM_HEADERS 1
