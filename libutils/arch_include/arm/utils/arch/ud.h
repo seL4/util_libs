fb4dc8bc2efdde93f010e0ed18f4e207078b2a22
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-09-01 13:55:39 +1000

    libutils: Support undefined instruction on armv8

--- libutils/arch_include/arm/utils/arch/ud.h
+++ libutils/arch_include/arm/utils/arch/ud.h
@@ -17,6 +17,9 @@
 #define UD_INSTRUCTION_SIZE 4
 
 static inline void ALWAYS_INLINE utils_undefined_instruction(void) {
-    /* See §A8.8.247 of the ARM ARM. */
-    asm volatile ("udf #0");
+    /* Section §A8.8.247 of the ARM manual says that 'udf' can be used to generate an
+       undefined instruction. Unfortunately this mnenomic is not present on armv8,
+       but the opcode is still presently undefined. To work around this we use the raw
+       opcode for the udf instruction, and rely on it still being undefined on armv8. */
+    asm volatile (".word 0xe7f0def0");
 }
