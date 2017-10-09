3bc36d44ce1bb34ba0252c512e751e4bf5737ff6
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-10-09 16:17:05 +1100

    libplatsupport: Correct format types for printf

--- libplatsupport/src/plat/pc99/pit.c
+++ libplatsupport/src/plat/pc99/pit.c
@@ -56,8 +56,8 @@ configure_pit(pit_t *pit, uint8_t mode, uint64_t ns)
     int error;
 
     if (ns > PIT_MAX_NS || ns < PIT_MIN_NS) {
-        ZF_LOGE("ns invalid for programming PIT %u <= %"PRIuPTR" <= %u\n",
-                (uint32_t) PIT_MIN_TICKS, ns, (uint32_t) PIT_MAX_TICKS);
+        ZF_LOGE("ns invalid for programming PIT %u <= %"PRIu64" <= %u\n",
+                (unsigned int) PIT_MIN_TICKS, ns, (unsigned int) PIT_MAX_TICKS);
         return EINVAL;
     }
 
