24e47a4eff0b552e3719d762a0fdea6d5d49f624
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-10-31 15:12:29 +1100

    cmake: Sort projects for deterministic builds

--- cmake-tool/projects.cmake
+++ cmake-tool/projects.cmake
@@ -15,6 +15,12 @@ cmake_minimum_required(VERSION 3.7.2)
 file(GLOB result RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}"
     projects/*/CMakeLists.txt
 )
+
+# We sort the results to ensure that builds are deterministic. Whilst build scripts
+# should not be written to need a particular order of globbed results here, it is
+# better to have reliable builds than random failures
+list(SORT result)
+
 foreach(file ${result})
     string(REPLACE "CMakeLists.txt" "" file "${file}")
     add_subdirectory("${file}")
