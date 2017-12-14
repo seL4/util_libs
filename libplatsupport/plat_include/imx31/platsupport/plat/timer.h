43a15c3f4e1310fee5668658bcf2065821adc5c8
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-12-14 17:29:34 +1100

    Use EPIT1 as a timestamp timer for i.MX31 for RT
    On the RT kernel, the kernel uses the GPT as opposed to master where the
    kernel uses EPIT1.

--- libplatsupport/plat_include/imx31/platsupport/plat/timer.h
+++ libplatsupport/plat_include/imx31/platsupport/plat/timer.h
@@ -11,17 +11,61 @@
  */
 #pragma once
 
+#include <autoconf.h>
 #include <platsupport/mach/gpt.h>
 #include <platsupport/mach/epit.h>
 
 #define IPG_FREQ (532/16) /*x10^5 */
 #define GPT_FREQ IPG_FREQ
 
+#ifdef CONFIG_KERNEL_RT
+/* for RT, we use the EPIT as timestamp timer as the kernel is using the GPT */
+#define TIMESTAMP_INTERRUPT EPIT1_INTERRUPT
+#define TIMESTAMP_DEVICE_PADDR EPIT1_DEVICE_PADDR
+
+typedef struct {
+    epit_t timestamp;
+    epit_t timeout;
+    uint64_t high_bits;
+} imx_timers_t;
+
+static inline void handle_irq_timestamp(imx_timers_t *timers)
+{
+    timers->high_bits++;
+    epit_handle_irq(&timers->timestamp);
+}
+
+static inline uint64_t imx_get_time(imx_timers_t *timers)
+{
+    uint64_t ticks = (timers->high_bits + !!epit_is_irq_raised(&timers->timestamp)) << 32llu;
+    ticks += (UINT32_MAX - epit_read(&timers->timestamp));
+    return epit_ticks_to_ns(&timers->timestamp, ticks);
+}
+
+static inline void imx_start_timestamp(imx_timers_t *timers)
+{
+    epit_set_timeout_ticks(&timers->timestamp, UINT32_MAX, true);
+}
+
+static inline void imx_stop_timestamp(imx_timers_t *timers)
+{
+    epit_stop(&timers->timestamp);
+}
+
+static inline int imx_init_timestamp(imx_timers_t *timers, void *vaddr)
+{
+    epit_config_t config = {
+        .vaddr = vaddr,
+        .irq = TIMESTAMP_INTERRUPT,
+        .prescaler = 0
+    };
+    return epit_init(&timers->timestamp, config);
+}
+
+#else
+/* for baseline, the timestamp timer is the GPT as the kernel is using EPIT1 */
 #define TIMESTAMP_INTERRUPT GPT1_INTERRUPT
 #define TIMESTAMP_DEVICE_PADDR GPT1_DEVICE_PADDR
-
-#define TIMEOUT_INTERRUPT EPIT2_INTERRUPT
-#define TIMEOUT_DEVICE_PADDR EPIT2_DEVICE_PADDR
 
 typedef struct {
     gpt_t timestamp;
@@ -56,6 +100,11 @@ static inline int imx_init_timestamp(void *vaddr)
     };
     return gpt_init(&timers->timestamp, config);
 }
+#endif
+
+/* for both kernel versions, we use EPIT2 as the timeout timer */
+#define TIMEOUT_INTERRUPT EPIT2_INTERRUPT
+#define TIMEOUT_DEVICE_PADDR EPIT2_DEVICE_PADDR
 
 static inline void handle_irq_timeout(imx_timers_t *timers)
 {
