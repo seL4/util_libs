022fcfe98947afbbbb54ec6a86eba6f5b7b4dd13
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-02-07 11:48:20 +1100

    cmake-tool: Dead code and data removal option
    Adds option for performing dead code and data removal through the use of function and
    data sections.

--- cmake-tool/flags.cmake
+++ cmake-tool/flags.cmake
@@ -24,6 +24,24 @@ mark_as_advanced(CLEAR CMAKE_BUILD_TYPE)
 # Setup base flags as defined by the kernel before including the rest
 include(${KERNEL_FLAGS_PATH})
 
+if ((${CMAKE_BUILD_TYPE} STREQUAL "Release") OR (${CMAKE_BUILD_TYPE} STREQUAL "MinSizeRel"))
+    option(UserLinkerGCSections "Perform dead code and data removal
+        Build user level with -ffunction-sections and -fdata-sections and
+        link with --gc-sections. The first two options place each function
+        and data in a different section such that --gc-sections is able
+        to effectively discard sections that are unused after a reachability
+        analysis. This does not interact well with debug symbols generated
+        by -g and can in some cases result in larger object files and binaries"
+
+        ON
+    )
+
+    if (UserLinkerGCSections)
+        add_compile_options(-ffunction-sections -fdata-sections)
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections ")
+    endif()
+endif()
+
 add_compile_options(-nostdinc -fno-pic -fno-pie -fno-stack-protector -fno-asynchronous-unwind-tables -ftls-model=local-exec)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nostdinc++")
 set(CMAKE_C_STANDARD 11)
