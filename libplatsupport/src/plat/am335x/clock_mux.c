101a7cbb59ac7a80f51dd1783cce072eba5551ce
Kofi Doku Atuah <kofidoku.atuah@data61.csiro.au>
2018-03-14 13:41:00 +1100

    platsupport: am355x: Fix build error

--- libplatsupport/src/plat/am335x/clock_mux.c
+++ libplatsupport/src/plat/am335x/clock_mux.c
@@ -10,6 +10,7 @@
  * @TAG(DATA61_BSD)
  */
 
+#include <utils/attribute.h>
 #include <platsupport/mux.h>
 #include <platsupport/clock.h>
 
@@ -20,7 +21,7 @@ clock_sys_init(ps_io_ops_t* io_ops, clock_sys_t* clk_sys)
 }
 
 int
-mux_sys_init(ps_io_ops_t* io_ops, mux_sys_t* mux)
+mux_sys_init(ps_io_ops_t* io_ops, UNUSED void *dependencies, mux_sys_t* mux)
 {
     return 0;
 }
