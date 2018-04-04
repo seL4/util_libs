13a5364d722e141bb753cea1a77e3cd7f6457ec0
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2018-04-04 14:36:55 +1000

    elfloader: Remove more deadcode

--- elfloader-tool/src/arch-riscv/boot.c
+++ elfloader-tool/src/arch-riscv/boot.c
@@ -20,73 +20,22 @@
 
 
 #define PTE_TYPE_TABLE 0x00
-#define PTE_TYPE_TABLE_GLOBAL 0x02
-#define PTE_TYPE_URX_SR 0x04
-#define PTE_TYPE_URWX_SRW 0x06
-#define PTE_TYPE_UR_SR 0x08
-#define PTE_TYPE_URW_SRW 0x0A
-#define PTE_TYPE_URX_SRX 0x0C
-#define PTE_TYPE_URWX_SRWX 0x0E
-#define PTE_TYPE_SR 0x10
-#define PTE_TYPE_SRW 0x12
-#define PTE_TYPE_SRX 0x14
 #define PTE_TYPE_SRWX 0xCE
-#define PTE_TYPE_SR_GLOBAL 0x18
-#define PTE_TYPE_SRW_GLOBAL 0x1A
-#define PTE_TYPE_SRX_GLOBAL 0x1C
-#define PTE_TYPE_SRWX_GLOBAL 0x1E
 
 #define RISCV_PGSHIFT 12
 #define RISCV_PGSIZE (1 << RISCV_PGSHIFT)
 
-// page table entry (PTE) fields
+// page table entry (PTE) field
 #define PTE_V     0x001 // Valid
-#define PTE_TYPE  0x01E // Type
-#define PTE_R     0x020 // Referenced
-#define PTE_D     0x040 // Dirty
-#define PTE_SOFT  0x380 // Reserved for Software
 
-#define PTE_PPN_SHIFT 10
-#define PTE_PPN1_SHIFT 20
-
-#define PTE64_PPN2_SHIFT 28
-#define PTE64_PPN1_SHIFT 19
 #define PTE64_PPN0_SHIFT 10
 
 #define PTES_PER_PT (RISCV_PGSIZE/sizeof(long))
 
-/* Virtual address to index conforming sv32 PTE format */
-#define VIRT1_TO_IDX(addr) ((addr) >> 22)
-#define VIRT0_TO_IDX(addr) (((addr) >> 12)
 
-#define SV39_VIRT_TO_VPN2(addr) ((addr) >> 30)
-#define SV39_VIRT_TO_VPN1(addr) ((addr) >> 21)
-#define SV39_VIRT_TO_VPN0(addr) ((addr) >> 12)
-
-#define PTE_CREATE(PPN, TYPE) (((PPN) << PTE_PPN_SHIFT) | (TYPE) | PTE_V)
 #define PTE64_CREATE(PPN, TYPE) (unsigned long) (((uint32_t)PPN) | (TYPE) | PTE_V)
 #define PTE64_PT_CREATE(PT_BASE) \
   (((unsigned long)((unsigned long) PT_BASE) / RISCV_PGSIZE) << 10 | PTE_TYPE_TABLE | PTE_V)
-
-#define write_csr(reg, val) \
-  asm volatile ("csrw " #reg ", %0" :: "r"(val))
-
-#define swap_csr(reg, val) ({ long __tmp; asm volatile ("csrrw %0, " #reg ", %1" : "=r"(__tmp) : "r"(val)); \
-  __tmp; })
-
-#define set_csr(reg, bit) ({ unsigned long __tmp; \
-  if (__builtin_constant_p(bit) && (bit) < 32) \
-  asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "i"(bit)); \
-  else \
-  asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "r"(bit)); \
-  __tmp; })
-
-#define clear_csr(reg, bit) ({ unsigned long __tmp; \
-  if (__builtin_constant_p(bit) && (bit) < 32) \
-  asm volatile ("csrrc %0, " #reg ", %1" : "=r"(__tmp) : "i"(bit)); \
-  else \
-    asm volatile ("csrrc %0, " #reg ", %1" : "=r"(__tmp) : "r"(bit)); \
-  __tmp; })
 
 struct image_info kernel_info;
 struct image_info user_info;
@@ -101,8 +50,6 @@ void
 map_kernel_window(struct image_info *kernel_info)
 {
     uint32_t i;
-    // paddr_t  phys;
-    // phys = SV39_VIRT_TO_VPN1(kernel_info->phys_region_start) & 0x1FF;
 
    for(i = 0; i < 20; i++)
  #if __riscv_xlen == 32
