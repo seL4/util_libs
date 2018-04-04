ec5140e50312307f9100f1b6c740c05c861ffd39
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2018-04-04 11:23:21 +1000

    common-tool: Remove dead code

--- common-tool/common.mk
+++ common-tool/common.mk
@@ -81,9 +81,6 @@ CONFIG_USER_CFLAGS:=$(patsubst %",%,$(patsubst "%,%,${CONFIG_USER_CFLAGS}))
 STARTGROUP := -Wl,--start-group
 ENDGROUP := -Wl,--end-group
 
-#STARTGROUP :=
-#ENDGROUP :=
-
 ifeq (${CONFIG_USER_CFLAGS},)
     CFLAGS += $(WARNINGS:%=-W%) -nostdinc -std=gnu11
     CXXFLAGS += $(WARNINGS:%=-W%) -nostdinc $(call cc-option,-std=gnu++14,-std=gnu++98)
