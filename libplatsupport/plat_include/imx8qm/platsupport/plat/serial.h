3f1bce7c64fb25acce9d28392427e6f50f67da87
Chris Guikema <chris.guikema@dornerworks.com>
2018-05-03 12:12:28 -0400

    Added blank register to match iMX8 UART Registers
    Co-authored-by: Nicholas Pahl <Nicholas.Pahl@DornerWorks.com>
    
    Change-Id: Ic00c0afaf7fd2bb3830b505566bca2afeb42403c

--- libplatsupport/plat_include/imx8qm/platsupport/plat/serial.h
+++ libplatsupport/plat_include/imx8qm/platsupport/plat/serial.h
@@ -52,6 +52,7 @@ struct imx8_uart_regs {
     uint32_t ctrl;     /* 0x018 Control Register */
     uint32_t data;     /* 0x01C Data Register */
     uint32_t match;    /* Match Address Register */
+    uint32_t res0;
     uint32_t fifo;     /* FIFO Register */
     uint32_t water;    /* Watermark Register */
 };
