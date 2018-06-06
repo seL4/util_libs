bd85105b48c7e34d3c46b59628ceeeb0f0871b83
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-06-06 17:45:42 +1000

    elfloader: add odroidc2

---
+++ elfloader-tool/src/plat/odroidc2/sys_fputc.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ */
+
+#include <printf.h>
+#include <types.h>
+#include <platform.h>
+#include <elfloader_common.h>
+
+#define UART_WFIFO  0x0
+#define UART_STATUS 0xC
+#define UART_TX_FULL        BIT(21)
+#define UART_REG(x) ((volatile uint32_t *)(UART_PPTR + (x)))
+
+int
+__fputc(int c, FILE *stream)
+{
+
+   while ((*UART_REG(UART_STATUS) & UART_TX_FULL));
+   *UART_REG(UART_WFIFO) = c;
+
+    /* Send '\r' after every '\n'. */
+    if (c == '\n') {
+        (void)__fputc('\r', stream);
+    }
+
+    return 0;
+}
