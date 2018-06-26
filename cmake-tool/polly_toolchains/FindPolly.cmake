698f676d12f67e0359783673d2237325c3ba9d18
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2018-06-26 16:43:53 +1000

    CMake: Add custom polly toolchain and helpers
    FindCustomPollyToolchain can be used to load a custom toolchain,
    typically to be used as an argument to an ExternalProject_add call

---
+++ cmake-tool/polly_toolchains/FindPolly.cmake
@@ -0,0 +1,22 @@
+#
+# Copyright 2018, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+cmake_minimum_required(VERSION 3.8.2)
+
+# Save path to checkout of https://github.com/ruslo/polly
+macro(FindPolly)
+	find_path(POLLY_DIR linux-gcc-x64.cmake ${CMAKE_CURRENT_LIST_DIR}/../../../../tools/polly)
+	mark_as_advanced(FORCE POLLY_DIR)
+	if("${POLLY_DIR}" STREQUAL "POLLY_DIR-NOTFOUND")
+	    message(FATAL_ERROR "Failed to find polly. Consider cmake -DPOLLY_DIR=/path/to/polly")
+	endif()
+endmacro()
---
+++ cmake-tool/polly_toolchains/linux-gcc-7-32bit-pic.cmake
@@ -0,0 +1,29 @@
+#
+# Copyright 2018, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+include(${CMAKE_CURRENT_LIST_DIR}/FindPolly.cmake)
+
+FindPolly()
+include("${POLLY_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+    "Linux / gcc-7 / PIC / c++11 support / 32 bit"
+    "Ninja"
+)
+
+include("${POLLY_DIR}/utilities/polly_common.cmake")
+
+include("${POLLY_DIR}/compiler/gcc-7.cmake")
+include("${POLLY_DIR}/flags/cxx11.cmake")
+include("${POLLY_DIR}/flags/fpic.cmake")
+include("${POLLY_DIR}/flags/32bit.cmake")
+
