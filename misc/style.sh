5bb27b2fff09f74d5c9937f5f6c993472fa93bbb
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-08-08 10:54:49 +1000

    misc: Added an astyle script
    Added a simple script to run astyle on a given directory.
    This is functionality ported from the Kbuild system.
    To invoke the user runs:
    "misc/style.sh -s=/dir/to/src".

---
+++ misc/style.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+# Copyright 2018, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+TOOLS_DIR=`dirname "$0"`
+
+for i in "$@"
+do
+case $i in
+    -s=*|--source-dir=*)
+    SOURCE_DIR="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+
+# Default to current directory if no directory is passed
+if [ -z "$SOURCE_DIR" ]; then
+    SOURCE_DIR=`pwd`
+fi
+
+echo "Styling directory: $SOURCE_DIR"
+
+astyle --options=$TOOLS_DIR/astylerc --recursive "$SOURCE_DIR/*.c" "$SOURCE_DIR/*.h"
