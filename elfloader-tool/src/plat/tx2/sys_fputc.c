e65661b96c8f08dcf238a9766812a85c75d02659
Peter Chubb <Peter.Chubb@data61.csiro.au>
2018-10-16 11:19:35 +1100

    TX2 initial port
    Copy most files from TX1, but TX2 u-boot wants a uImage to boot from.

---
+++ elfloader-tool/src/plat/tx2/sys_fputc.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ */
+
+#include <printf.h>
+#include <types.h>
+#include <platform.h>
+
+#define UTHR        0x0
+#define ULSR        0x14
+#define ULSR_THRE   (1 << 5)
+
+#define UART_REG(x) ((volatile uint32_t *)(UART_PPTR + (x)))
+
+int
+__fputc(int c, FILE *stream)
+{
+    /* Wait until UART ready for the next character. */
+    while ((*UART_REG(ULSR) & ULSR_THRE) == 0);
+
+    /* Add character to the buffer. */
+    *UART_REG(UTHR) = (c & 0xff);
+
+    /* Send '\r' after every '\n'. */
+    if (c == '\n') {
+        (void)__fputc('\r', stream);
+    }
+
+    return 0;
+}
