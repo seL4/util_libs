d95f31af312b3b2ae985985836d15360abc8d31a
Simon Shields <simon.shields@data61.csiro.au>
2018-11-26 14:44:56 +1100

    elfloader: add basic memmove implementation
    This is a dumb, unoptimised memmove implementation. It will
    use memcpy() where our memcpy implementation will not overwrite data,
    but falls back to doing a basic byte-at-a-time copy otherwise.

--- elfloader-tool/include/strops.h
+++ elfloader-tool/include/strops.h
@@ -18,5 +18,6 @@ size_t strlen(const char *str);
 int strcmp(const char *a, const char *b);
 int strncmp(const char *s1, const char *s2, size_t n);
 void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 
--- elfloader-tool/src/string.c
+++ elfloader-tool/src/string.c
@@ -96,6 +96,33 @@ void *memset(void *s, int c, size_t n)
     return s;
 }
 
+void *memmove(void *restrict dest, const void *restrict src, size_t n)
+{
+    unsigned char *d = (unsigned char *)dest;
+    const unsigned char *s = (const unsigned char *)src;
+
+    /* no copying to do */
+    if (d == s) {
+        return dest;
+    }
+    /* for non-overlapping regions, just use memcpy */
+    else if (s + n <= d || d + n <= s) {
+        return memcpy(dest, src, n);
+    }
+    /* if copying from the start of s to the start of d, just use memcpy */
+    else if (s > d) {
+        return memcpy(dest, src, n);
+    }
+
+    /* copy from end of 's' to end of 'd' */
+    size_t i;
+    for (i = 1; i <= n; i++) {
+        d[n - i] = s[n - i];
+    }
+
+    return dest;
+}
+
 void *memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
     unsigned char *d = (unsigned char *)dest;
