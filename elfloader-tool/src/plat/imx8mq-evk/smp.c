5c4db492d793d67ff84e5ec6ea1e7f181c9b8b61
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-03-22 10:34:01 +1100

    Add support for the i.MX8MQ Eval kit platform
    This adds support for the 64-bit i.MX8M Quad evaluation kit.
    Currently only AArch64 EL1 is supported.

---
+++ elfloader-tool/src/plat/imx8mq-evk/smp.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2019, DornerWorks
+ * Copyright 2019, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_DORNERWORKS_GPL)
+ */
+
+#include <autoconf.h>
+
+#if CONFIG_MAX_NUM_NODES > 1
+#error "SMP not implemented for imx8mq-evk"
+#endif /* CONFIG_MAX_NUM_NODES > 1 */
