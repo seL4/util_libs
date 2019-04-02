5d46023df00b7c736e172a03d86fac4afb860b42
G. Branden Robinson <Branden.Robinson@data61.csiro.au>
2019-04-02 13:17:06 +1100

    init-build.sh: Tidy up
    * Set interpreter to /bin/sh; there are no more non-POSIXisms in the script.
    * Set "set -u" flag to warn against use of undeclared variables.
    * Use more idiomatic (and much shorter) method of obtaining name of directory
      containing the script.
    * Use more idiomatic (and non-Bashistic) method of obtaining name of executing
      script.
    * Use more idiomatic method of referring to current working directory; the shell
      maintains $PWD, so one does not have to do process substitution with the "pwd"
      command.
    * Exit with status 1 on problems, not -1; "checkbashisms" warns on -1.
    * Send diagnostic messages to standard error.
    * Explain in a comment why we run cmake so many times.
    * Wrap lines at 80 columns.
    * Add Vim modeline.

--- cmake-tool/init-build.sh
+++ cmake-tool/init-build.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 #
-# Copyright 2017, Data61
+# Copyright 2019, Data61
 # Commonwealth Scientific and Industrial Research Organisation (CSIRO)
 # ABN 41 687 119 230.
 #
@@ -11,31 +11,37 @@
 # @TAG(DATA61_BSD)
 #
 
-# This script is intended to be symlinked into the same location as your root CMakeLists.txt file
-# and then invoked from a clean build directory
+# This script is intended to be symlinked into the same location as your root
+# CMakeLists.txt file and then invoked from a clean build directory.
 
-set -e
+set -eu
 
-# Determine script path (do not resolve symbolic links, we want it as given by the user)
-pushd . > /dev/null
-SCRIPT_PATH="${BASH_SOURCE[0]}"
-cd `dirname ${SCRIPT_PATH}`
-SCRIPT_PATH=`pwd`;
-popd > /dev/null
+# Determine path to this script (fast, cheap "dirname").
+SCRIPT_PATH=${0%/*}
+# Save script name for diagnostic messages (fast, cheap "basename").
+SCRIPT_NAME=${0##*/}
 
-# Ensure script path and current working directory are not the same
-if [ "`pwd`" = "${SCRIPT_PATH}" ]
+# Ensure script path and current working directory are not the same.
+if [ "$PWD" = "$SCRIPT_PATH" ]
 then
-    echo "${BASH_SOURCE[0]} should not be invoked from build directory":
-    exit -1
+    echo "\"$SCRIPT_NAME\" should not be invoked from top-level directory" >&2
+    exit 1
 fi
 
-# Try and make sure we weren't invoked from a source directory by checking for a CMakeLists.txt file
-if [ -e "`pwd`/CMakeLists.txt" ]
+# Try and make sure we weren't invoked from a source directory by checking for a
+# CMakeLists.txt file.
+if [ -e CMakeLists.txt ]
 then
-    echo "${BASH_SOURCE[0]} should be invoked from build directory and not source directories containing a CMakeLists.txt file"
-    exit -1
+    echo "\"$SCRIPT_NAME\" should be invoked from a build directory and not" \
+        "source directories containing a CMakeLists.txt file" >&2
+    exit 1
 fi
 
-# Initialize cmake
-cmake -DCMAKE_TOOLCHAIN_FILE=${SCRIPT_PATH}/kernel/gcc.cmake -G Ninja $@ ${SCRIPT_PATH} && cmake ${SCRIPT_PATH} && cmake ${SCRIPT_PATH}
+# Initialize CMake.
+#
+# We run cmake multiple times because project-specific mandatory settings are
+# sometimes buried deep in the tree of cmake files, and might get dereferenced
+# before they are used.  CMake's caching stabilizes the situation, at the cost
+# of requiring multiple runs.  Fixing this infelicity is JIRA SELFOUR-1648.
+cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_PATH"/kernel/gcc.cmake -G Ninja "$@" \
+    "$SCRIPT_PATH" && cmake "$SCRIPT_PATH" && cmake "$SCRIPT_PATH"
