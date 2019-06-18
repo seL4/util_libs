7d098a6e738f293f1e9c921883716df97ae50206
G. Branden Robinson <Branden.Robinson@data61.csiro.au>
2019-06-18 15:36:06 +1000

    style-cmake.sh: check version of cmake-format
    Our settings work correctly only with cmake-format 0.4.x.  0.5.x causes
    undesirable output.

--- misc/style-cmake.sh
+++ misc/style-cmake.sh
@@ -14,6 +14,8 @@
 
 # Style an input list of files as cmake files.
 
+PROGNAME=${0##*/}
+
 # Pass the cmake format as args such that cmake-format.py can be used
 # to provide definitions for custom function formatting.
 CMAKE_FMT="--line-width 100 \
@@ -25,4 +27,24 @@ CMAKE_FMT="--line-width 100 \
            --command-case unchanged \
            --keyword-case unchanged \
            --enable-markup False"
+
+# cmake-format sends its version info to standard error.  :-/
+CF_VERSION=$(cmake-format --version 2>&1)
+DESIRED_VERSION=0.4.5
+
+case "$CF_VERSION" in
+    ("")
+        echo "$PROGNAME: fatal error: no output from \"cmake-format --version\""
+        exit 2
+        ;;
+    ($DESIRED_VERSION)
+        # Good version; proceed.
+        ;;
+    (*)
+        echo "$PROGNAME: fatal error: need version $DESIRED_VERSION of"
+            \ "cmake-format; $CF_VERSION is installed"
+        exit 2
+        ;;
+esac
+
 cmake-format -i $CMAKE_FMT "$@"
