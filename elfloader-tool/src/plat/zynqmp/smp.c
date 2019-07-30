8f1ee6bdad8611fd2cc5fdb38434bd531a99b158
Chris Guikema <chris.guikema@dornerworks.com>
2019-07-30 10:14:38 -0400

    elfloader: add multicore support for zynqmp
    Add required logic for booting 4 cores on the zynqmp.

--- elfloader-tool/src/plat/zynqmp/smp.c
+++ elfloader-tool/src/plat/zynqmp/smp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, DornerWorks
+ * Copyright 2019, DornerWorks
  * Copyright 2017, Data61
  * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
  * ABN 41 687 119 230.
@@ -22,5 +22,39 @@
 #include <elfloader/gen_config.h>
 
 #if CONFIG_MAX_NUM_NODES > 1
-#error "SMP not implemented for zynqmp"
+
+#include <types.h>
+#include <elfloader.h>
+#include <armv/psci.h>
+#include <armv/machine.h>
+#include <armv/smp.h>
+#include <printf.h>
+#include <abort.h>
+
+#define MAX_CORES 4
+
+extern void core_entry_head(unsigned long stack);
+
+void init_cpus(void)
+{
+    int nodes = CONFIG_MAX_NUM_NODES;
+    if (nodes > MAX_CORES) {
+        printf("CONFIG_MAX_NUM_NODES %d is greater than max number cores %d, will abort\n",
+               CONFIG_MAX_NUM_NODES, MAX_CORES);
+        abort();
+    }
+    for (int i = 1; i < nodes; i++) {
+        int ret = psci_cpu_on((unsigned long)i, (unsigned long)core_entry_head, (unsigned long)&core_stacks[i][0]);
+        if (ret != PSCI_SUCCESS) {
+            printf("Failed to bring up core %d with error %x\n", i, ret);
+            abort();
+        }
+        while (!is_core_up(i));
+        printf("Core %d is up with logic ID %d\n", i, i);
+    }
+
+    /* set the logic id for the booting core */
+    MSR("tpidr_el1", 0);
+}
+
 #endif /* CONFIG_MAX_NUM_NODES > 1 */
