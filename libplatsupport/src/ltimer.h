5347ee196e85322c14684553cb05fb06e34a104f
Damon Lee <Damon.Lee@data61.csiro.au>
2019-08-02 11:20:14 +1000

    libplatsupport: Add IRQ callback helper for ltimer
    This commit introduces a new struct type and function that is intended
    to be passed to the IRQ interface as a callback for timer interrupts.

---
+++ libplatsupport/src/ltimer.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#include <platsupport/ltimer.h>
+#include <platsupport/irq.h>
+
+typedef struct {
+    ltimer_t *ltimer;
+    ps_irq_t *irq;
+} timer_callback_data_t;
+
+/*
+ * This is a simple wrapper around each platform's handle_irq.
+ *
+ * This is called as a callback function from the IRQ interface when the user asks it to handle IRQs.
+ */
+static inline void handle_irq_wrapper(void *data, ps_irq_acknowledge_fn_t acknowledge_fn, void *ack_data)
+{
+    assert(data);
+
+    timer_callback_data_t *callback_data = (timer_callback_data_t *) data;
+
+    ltimer_t *ltimer = callback_data->ltimer;
+    ps_irq_t *irq = callback_data->irq;
+
+    int error = ltimer->handle_irq(ltimer->data, irq);
+    assert(!error);
+
+    error = acknowledge_fn(ack_data);
+    assert(!error);
+}
