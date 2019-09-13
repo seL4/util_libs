c4d50a9934934d07dd586d9abae1a66c26f28f71
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-09-13 10:09:19 +1000

    CMake: Add Findutil_libs.cmake module
    This adds support for importing this project via
    `find_package(util_libs)` if the project exists in the
    CMAKE_MODULE_PATH.

---
+++ Findutil_libs.cmake
@@ -0,0 +1,21 @@
+#
+# Copyright 2019, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+set(UTIL_LIBS_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE STRING "")
+mark_as_advanced(UTIL_LIBS_DIR)
+
+macro(util_libs_import_libraries)
+    add_subdirectory(${UTIL_LIBS_DIR} util_libs)
+endmacro()
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(util_libs DEFAULT_MSG UTIL_LIBS_DIR)
