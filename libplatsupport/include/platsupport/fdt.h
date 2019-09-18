af39e3820ec59a13c603c28d683cd44354422102
Damon Lee <Damon.Lee@data61.csiro.au>
2019-09-18 14:43:34 +1000

    trivial: Add docs to FDT and IRQ chip parsing libs

--- libplatsupport/include/platsupport/fdt.h
+++ libplatsupport/include/platsupport/fdt.h
@@ -24,16 +24,90 @@
 #define READ_CELL(size, addr, offset) (size == 2 ? READ_CELL64(addr + (offset * sizeof(uint32_t))) : \
                                                    READ_CELL32(addr + (offset * sizeof(uint32_t))))
 
+/*
+ * Type of the callback function that is called for each device register instance
+ * during a call to ps_fdt_walk_registers.
+ *
+ * @param pmem Description of the device register instance.
+ * @param curr_num A number indicating the current register instance that we are currently at.
+ * @param num_regs The total number of register instances that the device that we are walking through has.
+ *
+ * @return 0 on success, otherwise an error code
+ */
 typedef int (*reg_walk_cb_fn_t)(pmem_region_t pmem, unsigned curr_num, size_t num_regs, void *token);
 
+/*
+ * Type of the callback function that is called for each device interrupt
+ * instance during a call to ps_fdt_walk_irqs.
+ *
+ * @param irq Description of the device interrupt instance.
+ * @param curr_num A number indicating the current interrupt instance that we are currently at.
+ * @param num_irqs The total number of interrupt instances that the device that we are walking through has.
+ *
+ * @return 0 on success, otherwise an error code
+ */
 typedef int (*irq_walk_cb_fn_t)(ps_irq_t irq, unsigned curr_num, size_t num_irqs, void *token);
 
+/* An internal private struct used by the utility functions in this library. */
 typedef struct ps_fdt_cookie ps_fdt_cookie_t;
 
+/*
+ * Given a devicetree path, this function will return a cookie that can be used in
+ * the other functions in this library.
+ *
+ * @param io_fdt An initialised IO FDT interface.
+ * @param malloc_ops An initialised malloc interface.
+ * @param path Devicetree path to the device for which you want to use the utility functions on.
+ * @param ret_cookie Storage that will have the pointer to the corresponding cookie written to.
+ *
+ * @returns 0 on success, otherwise -EINVAL, -ENOMEM, or one of the error codes in libfdt
+ */
 int ps_fdt_read_path(ps_io_fdt_t *io_fdt, ps_malloc_ops_t *malloc_ops, char *path, ps_fdt_cookie_t **ret_cookie);
 
+/*
+ * Cleans up a cookie that was initialised by ps_fdt_read_path.
+ *
+ * @param malloc_ops An initialised malloc interface.
+ * @param cookie A pointer to a initialised cookie.
+ *
+ * @returns 0 on success, otherwise -EINVAL
+ */
 int ps_fdt_cleanup_cookie(ps_malloc_ops_t *malloc_ops, ps_fdt_cookie_t *cookie);
+
+/*
+ * Walks the registers property of the device node (if any) corresponding to the
+ * cookie passed in and calls a callback function at each register instance of
+ * the field.
+ *
+ * @param io_fdt An initialised IO FDT interface.
+ * @param cookie A pointer to a initialised cookie.
+ * @param callback Pointer to a callback function that is called at each register instance of the property.
+ * @param token Pointer to be passed into the callback function when it is called.
+ *
+ * @returns 0 on success, otherwise:
+ *  - one of the error codes in libfdt
+ *  - -EINVAL
+ *  - the error returned by the callback function
+ */
 
 int ps_fdt_walk_registers(ps_io_fdt_t *io_fdt, ps_fdt_cookie_t *cookie, reg_walk_cb_fn_t callback, void *token);
 
+/*
+ * Walks the interrupts/interrupts-extended field of the device node (if any)
+ * corresponding to the cookie passed in and calls a callback function at each
+ * interrupt instance of the field. Note that depending on the interrupt parser
+ * modules available, this function may fail to parse the interrupt property.
+ *
+ * The modules can checked by looking inside the libplatsupport/src/arch/arm/irqchip/ folder.
+ *
+ * @param io_fdt An initialised IO FDT interface.
+ * @param cookie A pointer to a initialised cookie.
+ * @param callback Pointer to a callback function that is called at each interrupt instance of the property.
+ * @param token Pointer to be passed into the callback function when it is called.
+ *
+ * @returns 0 on success, otherwise:
+ *  - one of the error codes in libfdt
+ *  - -EINVAL or -ENOENT for no suitable interrupt parsing module
+ *  - the error returned by the callback function
+ */
 int ps_fdt_walk_irqs(ps_io_fdt_t *io_fdt, ps_fdt_cookie_t *cookie, irq_walk_cb_fn_t callback, void *token);
--- libplatsupport/src/irqchip.h
+++ libplatsupport/src/irqchip.h
@@ -18,6 +18,7 @@
 #include <platsupport/fdt.h>
 #include <utils/util.h>
 
+/* Macro to streamline process of declaring new interrupt parsing modules */
 #define DEFINE_IRQCHIP_PARSER(instance, compatible_strings, parser_func)  \
     static ps_irqchip_t instance = {                                      \
         .compatible_list = compatible_strings,                            \
@@ -25,14 +26,44 @@
     };                                                                    \
     USED SECTION("_ps_irqchips") ps_irqchip_t *instance##_ptr = &instance
 
+/*
+ * Expected function type of the interrupt parsing functions.
+ *
+ * Given offset of the device node in the FDT, this should read the interrupts
+ * property, parse it and call the callback function on each interrupt instance
+ * of the property.
+ *
+ * @param dtb_blob A blob of a platform's FDT.
+ * @param node_offset Offset to the device node to be parsed.
+ * @param intr_controller_phandle Phandle to the interrupt controller that handles the interrupts of the device.
+ * @param callback Pointer to a callback function that is called at each interrupt instance in the property.
+ * @param token Pointer to a token that is passed into the callback each time it is called.
+ *
+ * @returns 0 on success, otherwise an error code
+ */
 typedef int (*ps_irqchip_parse_fn_t)(char *dtb_blob, int node_offset, int intr_controller_phandle,
                                      irq_walk_cb_fn_t callback, void *token);
 
+/*
+ * Struct describing a IRQ parser module.
+ */
 typedef struct ps_irqchip {
+    /* Array of compatibility strings for interrupt
+     * controllers that the module can handle, should be NULL terminated, e.g.
+     * foo = { "abc", NULL } */
     char **compatible_list;
+    /* Pointer to the parser function for this module */
     ps_irqchip_parse_fn_t parser_fn;
 } ps_irqchip_t;
 
+/*
+ * Helper function that retrieves a pointer to the interrupts property of a device.
+ *
+ * @param dtb_blob A blob of a platform's FDT.
+ * @param node_offset Offset to the desired device node.
+ * @param is_extended Pointer to a boolean that will have true written to it if the 'interrupts-extended' property was found, false otherwise
+ * @param prop_len Pointer to an int that will have the total length of the interrupts property written to it
+ */
 static inline const void *get_interrupts_prop(char *dtb_blob, int node_offset, bool *is_extended, int *prop_len)
 {
     ZF_LOGF_IF(!is_extended || !prop_len, "Ret args are NULL!");
