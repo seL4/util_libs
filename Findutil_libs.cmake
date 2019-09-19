1c3ae456e30b8c45763a014db215a6395208cd3a
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-09-19 17:24:37 +1000

    CMake: Add LWIP_HELPERS to util_libs package
    This saves the path of the lwip_helpers.cmake file.

--- Findutil_libs.cmake
+++ Findutil_libs.cmake
@@ -11,11 +11,12 @@
 #
 
 set(UTIL_LIBS_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE STRING "")
-mark_as_advanced(UTIL_LIBS_DIR)
+set(LWIP_HELPERS "${CMAKE_CURRENT_LIST_DIR}/liblwip/lwip_helpers.cmake" CACHE STRING "")
+mark_as_advanced(UTIL_LIBS_DIR LWIP_HELPERS)
 
 macro(util_libs_import_libraries)
     add_subdirectory(${UTIL_LIBS_DIR} util_libs)
 endmacro()
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(util_libs DEFAULT_MSG UTIL_LIBS_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(util_libs DEFAULT_MSG UTIL_LIBS_DIR LWIP_HELPERS)
