75a2cbe9d9e490cb56ec56797a4f4dc86105c98b
Damon Lee <Damon.Lee@data61.csiro.au>
2019-10-02 12:08:01 +1000

    trivial: Style changed files

--- libplatsupport/arch_include/arm/platsupport/clock.h
+++ libplatsupport/arch_include/arm/platsupport/clock.h
@@ -35,9 +35,9 @@ enum clock_gate_mode {
 };
 
 struct clock_sys {
-    clk_t* (*get_clock)(clock_sys_t* clock_sys, enum clk_id id);
-    int (*gate_enable)(clock_sys_t* clock_sys, enum clock_gate gate, enum clock_gate_mode mode);
-    void* priv;
+    clk_t *(*get_clock)(clock_sys_t *clock_sys, enum clk_id id);
+    int (*gate_enable)(clock_sys_t *clock_sys, enum clock_gate gate, enum clock_gate_mode mode);
+    void *priv;
 };
 
 #include <platsupport/io.h>
@@ -45,26 +45,26 @@ struct clock_sys {
 struct clock {
     enum clk_id id;
     /// Name of the clock
-    const char* name;
+    const char *name;
     /// Clock specific private data
     void *priv;
     /// The requested frequency (not the actual frequency)
     freq_t req_freq;
     /* For requesting a freq change up the tree. This should
      * be NULL until clk_register_child has been called */
-    clk_t* parent;
+    clk_t *parent;
     /// Provide linked list for this clock's parent.
-    clk_t* sibling;
+    clk_t *sibling;
     /// For signalling a freq change down the tree
-    clk_t* child;
+    clk_t *child;
     /* Changing parents and initialisation requires access to the clock
      * subsystem. A reference is stored here for convenience */
-    clock_sys_t* clk_sys;
+    clock_sys_t *clk_sys;
     /// Clock specific functions
-    clk_t* (*init)(clk_t* clk);
-    freq_t (*get_freq)(clk_t*);
-    freq_t (*set_freq)(clk_t*, freq_t hz);
-    void (*recal)(clk_t*);
+    clk_t *(*init)(clk_t *clk);
+    freq_t (*get_freq)(clk_t *);
+    freq_t (*set_freq)(clk_t *, freq_t hz);
+    void (*recal)(clk_t *);
 };
 
 /**
@@ -72,7 +72,7 @@ struct clock {
  * @param[in] clock_sys  A handle to the clock subsystem
  * @return               1 if the structure is valid;
  */
-static inline int clock_sys_valid(const clock_sys_t* clock_sys)
+static inline int clock_sys_valid(const clock_sys_t *clock_sys)
 {
     return clock_sys && clock_sys->priv;
 }
@@ -85,7 +85,7 @@ static inline int clock_sys_valid(const clock_sys_t* clock_sys)
  *                       to the clocking subsystem
  * @return               0 on success
  */
-int clock_sys_init(ps_io_ops_t* io_ops, clock_sys_t* clock_sys);
+int clock_sys_init(ps_io_ops_t *io_ops, clock_sys_t *clock_sys);
 
 /**
  * Initialise a clock subsystem that does not support clock configuration
@@ -97,7 +97,7 @@ int clock_sys_init(ps_io_ops_t* io_ops, clock_sys_t* clock_sys);
  *                       to the clocking subsystem
  * @return               0 on success
  */
-int clock_sys_init_default(clock_sys_t* clock_sys);
+int clock_sys_init_default(clock_sys_t *clock_sys);
 
 /**
  * Override the default frequency for a clock.
@@ -116,9 +116,9 @@ int clock_sys_set_default_freq(enum clk_id id, freq_t hz);
  * @return               On success, a handle to the acquired clock.
  *                       Otherwise, NULL.
  */
-static inline clk_t* clk_get_clock(clock_sys_t* clock_sys, enum clk_id id)
+static inline clk_t *clk_get_clock(clock_sys_t *clock_sys, enum clk_id id)
 {
-    clk_t * clk;
+    clk_t *clk;
     assert(clock_sys);
     assert(clock_sys->get_clock);
     clk = clock_sys->get_clock(clock_sys, id);
@@ -129,7 +129,7 @@ static inline clk_t* clk_get_clock(clock_sys_t* clock_sys, enum clk_id id)
  * prints a list of initialised clocks
  * @param[in] clock_sys  A handle to the clock subsystem
  */
-void clk_print_clock_tree(clock_sys_t* clock_sys);
+void clk_print_clock_tree(clock_sys_t *clock_sys);
 
 /**
  * Configure the gating mode of a clock
@@ -139,7 +139,7 @@ void clk_print_clock_tree(clock_sys_t* clock_sys);
  * @return               0 on success;
 
  */
-static inline int clk_gate_enable(clock_sys_t* clock_sys, enum clock_gate gate,
+static inline int clk_gate_enable(clock_sys_t *clock_sys, enum clock_gate gate,
                                   enum clock_gate_mode mode)
 {
     assert(clock_sys);
@@ -154,7 +154,7 @@ static inline int clk_gate_enable(clock_sys_t* clock_sys, enum clock_gate gate,
  * @return           The hz the clock was set to
  *                   (may not exactly match input param)
  */
-static inline freq_t clk_set_freq(clk_t* clk, freq_t hz)
+static inline freq_t clk_set_freq(clk_t *clk, freq_t hz)
 {
     assert(clk);
     assert(clk->set_freq);
@@ -168,7 +168,7 @@ static inline freq_t clk_set_freq(clk_t* clk, freq_t hz)
  * @return           The hz the clock was set to
  *                   (may not exactly match input param)
  */
-static inline freq_t clk_get_freq(clk_t* clk)
+static inline freq_t clk_get_freq(clk_t *clk)
 {
     assert(clk);
     assert(clk->get_freq);
@@ -182,7 +182,7 @@ static inline freq_t clk_get_freq(clk_t* clk)
  * @param[in] parent  The parent of this clock relationship
  * @param[in] child   The child of this clock relationship
  */
-void clk_register_child(clk_t* parent, clk_t* child);
+void clk_register_child(clk_t *parent, clk_t *child);
 
 /**
  * Initialise a clock structure which reports a fixed frequency
