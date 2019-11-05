e935f281157b963224b5881d9d2bca6df60a0254
Siwei Zhuang <siwei.zhuang@data61.csiro.au>
2019-11-05 12:21:56 +1100

    libplatsupport: Add support for Ariane
    Added placeholders in libplatsupport for Ariane to pass the compilation.
    The actual driver is yet to be implemented.

---
+++ libplatsupport/src/plat/ariane/chardev.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2019, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#include "../../chardev.h"
+#include "../../common.h"
+#include <utils/util.h>
+
+struct ps_chardevice *
+ps_cdev_init(enum chardev_id id, const ps_io_ops_t *o, struct ps_chardevice *d)
+{
+    return NULL;
+}
+
