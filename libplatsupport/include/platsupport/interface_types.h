7790daa0b5952956bac41e701b02695f2fac21da
Damon Lee <Damon.Lee@data61.csiro.au>
2020-03-23 12:19:38 +1100

    libplatsupport: Add new interface types
    This commit adds in extra interface type definitions for the interface
    registration service.

--- libplatsupport/include/platsupport/interface_types.h
+++ libplatsupport/include/platsupport/interface_types.h
@@ -14,6 +14,12 @@
 
 typedef enum ps_interface_type {
     PS_NULL_INTERFACE,
+    /* This ID represents the TX2 BPMP interface in libtx2bpmp inside the project_libs repository */
+    TX2_BPMP_INTERFACE,
+    /* These IDs represent the respective interfaces inside libplatsupport */
+    PS_GPIO_INTERFACE,
+    PS_CLOCK_INTERFACE,
+    PS_RESET_INTERFACE
     /* There are plans to add more interface types, but we need to first define
      * the interfaces. */
 } ps_interface_type_t;
