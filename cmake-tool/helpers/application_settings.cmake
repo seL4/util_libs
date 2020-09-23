ffe3305d8d3926ccfa0fa8019063c786b75850d6
Siwei Zhuang <siwei.zhuang@data61.csiro.au>
2020-09-23 16:22:31 +1000

    zynqmp: Specify entry address for SD card boot
    Assign a start address, making sure there's no conflict in the bootable
    binary.
    
    Signed-off-by: Siwei Zhuang <siwei.zhuang@data61.csiro.au>

--- cmake-tool/helpers/application_settings.cmake
+++ cmake-tool/helpers/application_settings.cmake
@@ -41,6 +41,9 @@ function(ApplyData61ElfLoaderSettings kernel_platform kernel_sel4_arch)
         # This is preserving what the Hikey's bootloader requires.
         set(IMAGE_START_ADDR 0x1000 CACHE INTERNAL "" FORCE)
     endif()
+    if(KernelPlatformZynqmp AND KernelSel4ArchAarch32)
+        set(IMAGE_START_ADDR 0x8000000 CACHE INTERNAL "" FORCE)
+    endif()
 endfunction()
 
 function(ApplyCommonSimulationSettings kernel_arch)
