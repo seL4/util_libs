f059193f01b363a2b7be8969b4b9908e331b332c
Curtis Millar <curtis.millar@data61.csiro.au>
2020-11-13 15:27:50 +1100

    Add base64 streaming encoder to libutils
    This adds a streaming base64 binary encoder to libutils that can be used
    to write base64 binary data to any `FILE *` continuously.

---
+++ libutils/include/utils/base64.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2020, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <utils/arith.h>
+
+/* A streaming base64 encoder */
+
+typedef struct {
+    FILE *output;
+    uint16_t buffer;
+    size_t bits;
+} base64_t;
+
+#define BASE64_LOOKUP (\
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" \
+    "abcdefghijklmnopqrstuvwxyz" \
+    "0123456789+/" \
+)
+
+/* Create a new base64 streamer that streams to the given output */
+static inline base64_t base64_new(FILE *output)
+{
+    return (base64_t) {
+        .output = output,
+        .buffer = 0,
+        .bits = 0,
+    };
+}
+
+/* Lookup the character for a given bit pattern */
+static inline uint8_t base64_lookup(uint8_t bit)
+{
+    return BASE64_LOOKUP[bit & MASK(6)];
+}
+
+/* Write a byte to a base64 stream */
+static inline int base64_putbyte(base64_t *streamer, uint8_t byte)
+{
+    /* Buffer the byte */
+    streamer->buffer <<= 8;
+    streamer->buffer |= byte;
+    streamer->bits += 8;
+
+    /* Write any bits to the output */
+    while (streamer->bits >= 6) {
+        streamer->bits -= 6;
+        uint8_t part = streamer->buffer >> streamer->bits;
+        fputc(base64_lookup(part), streamer->output);
+    }
+
+    return 0;
+}
+
+/* Write any remaining data to the output */
+static inline int base64_terminate(base64_t *streamer)
+{
+    if (streamer->bits > 0) {
+        size_t padding = 6 - streamer->bits;
+        streamer->buffer <<= padding;
+        fputc(base64_lookup(streamer->buffer), streamer->output);
+        while (padding > 0) {
+            fputc('=', streamer->output);
+            padding -= 2;
+        }
+    }
+
+    /* Reset the streamer */
+    streamer->bits = 0;
+
+    return 0;
+}
