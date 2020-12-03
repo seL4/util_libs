b6f99879e59950f39ee35472ab15eac1efb0f139
Lukas Graber <lukas.graber@hensoldt-cyber.com>
2020-12-03 08:22:25 +0100

    Bring Raspberry Pi 4 (RPi4) support
    Signed-off-by: Lukas Graber <lukas.graber@hensoldt-cyber.de>

---
+++ libplatsupport/src/plat/bcm2711/serial.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2017, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright (C) 2021, Hensoldt Cyber GmbH
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "../../chardev.h"
+
+int uart_init(const struct dev_defn *defn, const ps_io_ops_t *ops, ps_chardevice_t *dev);
