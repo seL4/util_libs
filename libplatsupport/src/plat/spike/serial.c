7a928a0cff5cd714d21d31186629e3edf8a6bfcf
Axel Heider <axelheider@gmx.de>
2021-03-06 17:21:29 +0100

    trivial: style
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- libplatsupport/src/plat/spike/serial.c
+++ libplatsupport/src/plat/spike/serial.c
@@ -9,9 +9,10 @@
 #include <string.h>
 
 
-int uart_init(const struct dev_defn* defn,
-              const ps_io_ops_t* ops,
-              ps_chardevice_t* dev)
+int uart_init(
+    const struct dev_defn *defn UNUSED,
+    const ps_io_ops_t *ops UNUSED,
+    ps_chardevice_t *dev UNUSED)
 {
     return 0;
 }
