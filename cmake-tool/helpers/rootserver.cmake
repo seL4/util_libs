4dd40103bb947bd1f92e5d199fdec08f62fdacf0
Axel Heider <axelheider@gmx.de>
2021-03-11 14:12:57 +0100

    elfloader: use CMake variable KernelWordSize
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- cmake-tool/helpers/rootserver.cmake
+++ cmake-tool/helpers/rootserver.cmake
@@ -117,8 +117,8 @@ function(DeclareRootserver rootservername)
                         ${CMAKE_OBJCOPY} -O binary "${elf_target_file}"
                         "${OPENSBI_BINARY_DIR}/payload"
                     COMMAND
-                        PLATFORM_RISCV_XLEN=${mode} CROSS_COMPILE=${CROSS_COMPILER_PREFIX} make -C
-                        "${OPENSBI_PATH}" O="${OPENSBI_BINARY_DIR}"
+                        PLATFORM_RISCV_XLEN=${KernelWordSize} CROSS_COMPILE=${CROSS_COMPILER_PREFIX}
+                        make -C "${OPENSBI_PATH}" O="${OPENSBI_BINARY_DIR}"
                         PLATFORM="${KernelOpenSBIPlatform}"
                         FW_PAYLOAD_PATH="${OPENSBI_BINARY_DIR}/payload"
                     DEPENDS "${elf_target_file}" elfloader ${USES_TERMINAL_DEBUG}
