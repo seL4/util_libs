535cc12d7065b21e485de655fbd52738ad156258
Kent McLeod <kent@kry10.com>
2021-11-17 11:21:32 +1100

    libethdrivers,imx: Reintroduce ethif_imx6_init
    This init function is used by ethif_new_lwip_driver and was previously
    removed in an earlier refactor. This commit reintroduces it.
    
    Signed-off-by: Kent McLeod <kent@kry10.com>

--- libethdrivers/include/ethdrivers/imx6.h
+++ libethdrivers/include/ethdrivers/imx6.h
@@ -7,4 +7,15 @@
 
 #pragma once
 
-/* nothing here */
+#include <platsupport/io.h>
+#include <ethdrivers/raw.h>
+
+/**
+ * This function initialises the hardware and conforms to the ethif_driver_init
+ * type in raw.h
+ * @param[out] eth_driver   Ethernet driver structure to fill out
+ * @param[in] io_ops        A structure containing os specific data and
+ *                          functions.
+ * @param[in] config        Platform Specific configuration data
+ */
+int ethif_imx6_init(struct eth_driver *eth_driver, ps_io_ops_t io_ops, void *config);
