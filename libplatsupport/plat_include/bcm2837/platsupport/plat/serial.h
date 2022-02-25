1ae31c723be0a0bceb5bbe301b074abfcc18ba12
SaarloosJohn <saarloos@msn.cn>
2022-02-25 11:38:50 +0800

    libplatsupport,bcm2837: fix UART interrupt number
    Signed-off-by: SaarloosJohn <saarloos@msn.cn>

--- libplatsupport/plat_include/bcm2837/platsupport/plat/serial.h
+++ libplatsupport/plat_include/bcm2837/platsupport/plat/serial.h
@@ -12,10 +12,16 @@
 #define UART_BUSADDR                0x7E215000
 
 #define UART_PADDR_0                (UART_BUSADDR-BUS_ADDR_OFFSET+PADDDR_OFFSET)
-/* Broadcom 2835 Peripheral Manual, section 7.5,
- * table "ARM Peripherals interrupts table"
+/* The mini-UART from the Auxiliary peripheral triggers interrupt 29 ("Aux int")
+ * on the VideoCore, which then signals pending GPU interrupts to the ARM core.
+ * Further details can be found in the Broadcom BCM2835/BCM2837 Manual, section
+ * 7.5, table "ARM peripherals interrupts table". The seL4 kernel's interrupt
+ * controller driver creates a linear mapping of all interrupts in the system
+ * (see include/drivers/bcm2836-armctrl-ic.h) with 32 ARM PPIs, 32 basic
+ * interrupts and 64 GPU interrupts. Thus the actual interrupt to be used in
+ * userland is 93 (=32+32+29).
  */
-#define UART_IRQ_0                  (57)
+#define UART_IRQ_0                  (93)
 
 enum chardev_id {
     BCM2837_UART0,
