664df3c73a469aec8e22c862a27ade860845e7d5
Joonas Onatsu <joonas.onatsu@unikie.com>
2022-07-26 14:27:56 +0300

    libplatsupport,rpi3/4: Generic serial drivers
    Both RPi 3 and 4 use the same mini-UART/PL011
    HW devices, so move the drivers to more generic
    machine directory under 'mach/bcm'. Refactored
    other source files/header accordingly to work
    with moved files.
    
    This also includes some updates/fixes to drivers, mainly
    fixed mini-UART/PL011 not working with virtio console
    due to missing/broken interrupt handling, and polling
    'getchar()' implementations. General cleanup/refactoring/styling
    of driver code, more elaborate commenting. Added preprocessor
    macro guards for platform-specific parts.
    
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>

--- libplatsupport/plat_include/bcm2837/platsupport/plat/serial.h
+++ libplatsupport/plat_include/bcm2837/platsupport/plat/serial.h
@@ -1,37 +1,79 @@
 /*
- * Copyright 2017, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright (C) 2021, Hensoldt Cyber GmbH
+ * Copyright 2022, Technology Innovation Institute
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#define BUS_ADDR_OFFSET             0x7E000000
-#define PADDDR_OFFSET               0x3F000000
+#include <autoconf.h>
+#include <platsupport/gen_config.h>
 
-#define UART_BUSADDR                0x7E215000
+/*
+ * On the Raspberry Pi platforms two processing units are at play: the
+ * VideoCore GPU and the ARM CPU. Both have different views on the address
+ * space, meaning different base addresses(VC: 0x7e000000, ARM: 0x3f000000).
+ * In TRMs and DTS files, generally the VideoCore view is used, resulting in
+ * peripherals being listed with their respective VideoCore base address.
+ * Since user code is running on the ARM cores, we are more interested in
+ * the ARM peripheral addresses.
+ * 
+ * BCM2837 UARTs live under "/soc/" bus.
+ * mini-UART is under AUX peripheral block.
+ * Access needs to be page aligned.
+ * 
+ * UART0 -> /soc/serial@7e201000
+ * UART1 -> /soc/aux@7e215000
+ *          -> /soc/serial@7e215040
+ */
 
-#define UART_PADDR_0                (UART_BUSADDR-BUS_ADDR_OFFSET+PADDDR_OFFSET)
-/* The mini-UART from the Auxiliary peripheral triggers interrupt 29 ("Aux int")
- * on the VideoCore, which then signals pending GPU interrupts to the ARM core.
- * Further details can be found in the Broadcom BCM2835/BCM2837 Manual, section
- * 7.5, table "ARM peripherals interrupts table". The seL4 kernel's interrupt
+#define PL011_UART_BASE  0x3f201000
+#define MINI_UART_BASE   0x3f215000
+
+#define UART0_OFFSET     0x0     // UART0: PL011
+#define UART1_OFFSET     0x40    // UART1: Mini UART
+
+#define UART0_PADDR      (PL011_UART_BASE)   // 0x3f201000
+#define UART1_PADDR      (MINI_UART_BASE)    // 0x3f215040
+
+
+/* BCM2835/2837 TRM 
+ * Section 7.5
+ * The mini-UART from the AUX peripheral triggers IRQ 29 ("Aux int")
+ * on the VideoCore, which then signals pending GPU interrupts 
+ * to the ARM core.
+ * 
+ * The PL011 triggers IRQ 57 ("uart_int").
+ * 
+ * The seL4 kernel's interrupt
  * controller driver creates a linear mapping of all interrupts in the system
  * (see include/drivers/bcm2836-armctrl-ic.h) with 32 ARM PPIs, 32 basic
- * interrupts and 64 GPU interrupts. Thus the actual interrupt to be used in
- * userland is 93 (=32+32+29).
+ * interrupts and 64 GPU interrupts. 
+ * 
+ * Thus, the actual interrupts to be used in userland are: 93 (=32+32+29).
+ * ->
+ *   mini-UART 32+32+29 = 93
+ *   PL011 UART 32+32+57 = 121
  */
-#define UART_IRQ_0                  (93)
+#define UART0_IRQ  121
+#define UART1_IRQ  93
+
+#define DEFAULT_SERIAL_PADDR      UART1_PADDR
+#define DEFAULT_SERIAL_INTERRUPT  UART1_IRQ
+
 
 enum chardev_id {
-    BCM2837_UART0,
+
+    BCM2xxx_UART0,
+    BCM2xxx_UART1,
 
     NUM_CHARDEV,
-    /* Aliases */
-    PS_SERIAL0 = BCM2837_UART0,
-    /* defaults */
-    PS_SERIAL_DEFAULT = BCM2837_UART0
-};
 
-#define DEFAULT_SERIAL_PADDR UART_PADDR_0
-#define DEFAULT_SERIAL_INTERRUPT UART_IRQ_0
+    /* Aliases */
+    PS_SERIAL0 = BCM2xxx_UART0,
+    PS_SERIAL1 = BCM2xxx_UART1,
+
+    /* Defaults */
+    PS_SERIAL_DEFAULT = BCM2xxx_UART1
+};
--- libplatsupport/src/plat/bcm2837/serial.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2017, Data61, CSIRO (ABN 41 687 119 230)
- *
- * SPDX-License-Identifier: BSD-2-Clause
- */
-
-#include "../../chardev.h"
-
-int uart_init(const struct dev_defn* defn, const ps_io_ops_t* ops, ps_chardevice_t* dev);
