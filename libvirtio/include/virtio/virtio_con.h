559b495fd6986364f1eaa8e46e09c271aa2b5b9d
Andy Bui <andy.bui@unsw.edu.au>
2022-08-03 05:26:06 +0000

    virtio: Support virtio console
    Add the virtio console header file.
    
    Signed-off-by: Andy Bui <andy.bui@unsw.edu.au>

---
+++ libvirtio/include/virtio/virtio_con.h
@@ -0,0 +1,82 @@
+#pragma once
+/*
+ * This header is BSD licensed so
+ * anyone can use the definitions to implement compatible drivers/servers:
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of IBM nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL IBM OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Copyright (C) Red Hat, Inc., 2009, 2010, 2011
+ * Copyright (C) Amit Shah <amit.shah@redhat.com>, 2009, 2010, 2011
+ */
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdint.h>
+
+#define CTL_RX_QUEUE 2
+#define CTL_TX_QUEUE 3
+
+/* Feature bits */
+#define VIRTIO_CON_F_SIZE           0   /* Does host provide console size? */
+#define VIRTIO_CON_F_MULTIPORT      1   /* Does host provide multiple ports? */
+#define VIRTIO_CON_F_EMERG_WRITE    2   /* Does host support emergency write? */
+
+#define VIRTIO_CON_BAD_ID (~(uint32_t)0)
+
+#define VIRTIO_CON_MAX_PORTS 4 /* Support max 4 ports right now... */
+
+#define VIRTIO_CON_CFG_MAX_PORTS (VIRTIO_PCI_CONFIG_OFF(false) + offsetof(struct virtio_con_cfg, max_nr_ports))
+
+struct virtio_con_cfg {
+    /* colums of the screens */
+    uint16_t cols;
+    /* rows of the screens */
+    uint16_t rows;
+    /* max. number of ports this device can hold */
+    uint32_t max_nr_ports;
+    /* emergency write register */
+    uint32_t emerg_wr;
+} __attribute__((packed));
+
+/*
+ * A message that's passed between the Host and the Guest for a
+ * particular port.
+ */
+struct virtio_con_ctl {
+    uint32_t id;    /* Port number */
+    uint16_t event; /* The kind of control event (see below) */
+    uint16_t value; /* Extra information for the key */
+} __attribute__((packed));
+
+/* Some events for control messages */
+#define VIRTIO_CON_DEVICE_READY 0
+#define VIRTIO_CON_PORT_ADD     1
+#define VIRTIO_CON_PORT_REMOVE  2
+#define VIRTIO_CON_PORT_READY   3
+#define VIRTIO_CON_CON_PORT     4
+#define VIRTIO_CON_RESIZE       5
+#define VIRTIO_CON_PORT_OPEN    6
+#define VIRTIO_CON_PORT_NAME    7
