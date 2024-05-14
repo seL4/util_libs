f98dad12344a4c0fecf37bd11c82692ecc7a4a9a
Damon Lee <damon@kry10.com>
2024-05-14 14:00:25 +1000

    libplatsupport: Add support for imx8mp-evk
    The i.MX 8M Plus Evaluation Kit is similar to the other platforms in the
    i.MX 8M family and shares the same serial and timer peripherals.
    
    Signed-off-by: Damon Lee <damon@kry10.com>

--- libplatsupport/plat_include/imx8m/platsupport/plat/gpt_constants.h
+++ libplatsupport/plat_include/imx8m/platsupport/plat/gpt_constants.h
@@ -6,8 +6,13 @@
 
 #pragma once
 
+#ifdef USE_IMX8MP_GPT_PATH
+#define GPT1_PATH "/soc@0/bus@30000000/timer@302d0000"
+#define GPT2_PATH "/soc@0/bus@30000000/timer@302e0000"
+#else
 #define GPT1_PATH "/gpt@302d0000"
 #define GPT2_PATH "/gpt@302e0000"
+#endif
 
 #define GPT_PRESCALER       (0)
 
