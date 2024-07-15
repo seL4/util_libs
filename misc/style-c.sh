91c87959b9730407a98803e5e7560835d65276aa
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-07-15 12:26:55 +1000

    misc: check that astyle exists
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- misc/style-c.sh
+++ misc/style-c.sh
@@ -4,6 +4,13 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
+set -e
+
+if ! command -v astyle >/dev/null 2>&1
+then
+    echo "astyle could not be found, it must be available to run the script."
+    exit 1
+fi
 
 # Format (in place) a list of files as C code.
 astyle --options="${0%/*}/astylerc" "$@"
